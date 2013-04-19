e35bc04d9546a95b46cc80ca03a9dc13e9304637
Hector Palacios <hector.palacios@digi.com>
2013-04-19 17:57:10 +0200

    cpx2: added platform cloned from ccardimx28
    Platform: Digi ConnectPort X2.
    Created under board/digi/ vendor.
    
    https://jira.digi.com/browse/DUB-162
    
    Signed-off-by: Hector Palacios <hector.palacios@digi.com>

---
+++ board/digi/cpx2/cpx2.c
@@ -0,0 +1,168 @@
+/*
+ * Digi ConnectCard for i.MX28 board
+ *
+ * Copyright (C) 2013 Digi International Inc.
+ *
+ * Based on mx28evk.c:
+ * Copyright (C) 2011 Marek Vasut <marek.vasut@gmail.com>
+ * on behalf of DENX Software Engineering GmbH
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <common.h>
+#include <asm/gpio.h>
+#include <asm/io.h>
+#include <asm/arch/imx-regs.h>
+#include <asm/arch/iomux-mx28.h>
+#include <asm/arch/clock.h>
+#include <asm/arch/sys_proto.h>
+#include <linux/mii.h>
+#include <miiphy.h>
+#include <netdev.h>
+#include <errno.h>
+#include "../common/cmd_nvram/lib/include/nvram.h"
+
+DECLARE_GLOBAL_DATA_PTR;
+
+/*
+ * Functions
+ */
+int board_early_init_f(void)
+{
+	/* Pull LCD_RS gpio down to enable FET that blocks 3V3 and 1V8
+	 * power regulators on module
+	 */
+	mxs_iomux_setup_pad(MX28_PAD_LCD_RS__GPIO_1_26 |
+			MXS_PAD_4MA | MXS_PAD_3V3 | MXS_PAD_PULLUP);
+	gpio_direction_output(MX28_PAD_LCD_RS__GPIO_1_26, 0);
+
+	/* IO0 clock at 480MHz */
+	mx28_set_ioclk(MXC_IOCLK0, 480000);
+	/* IO1 clock at 480MHz */
+	mx28_set_ioclk(MXC_IOCLK1, 480000);
+
+	/* SSP0 clock at 96MHz */
+	mx28_set_sspclk(MXC_SSPCLK0, 96000, 0);
+	/* SSP2 clock at 160MHz */
+	mx28_set_sspclk(MXC_SSPCLK2, 160000, 0);
+
+#ifdef	CONFIG_CMD_USB
+	mxs_iomux_setup_pad(MX28_PAD_SSP2_SS1__USB1_OVERCURRENT);
+	mxs_iomux_setup_pad(MX28_PAD_AUART2_RX__GPIO_3_8 |
+			MXS_PAD_4MA | MXS_PAD_3V3 | MXS_PAD_NOPULL);
+	gpio_direction_output(MX28_PAD_AUART2_RX__GPIO_3_8, 1);
+#endif
+
+	return 0;
+}
+
+int dram_init(void)
+{
+	return mxs_dram_init();
+}
+
+int board_init(void)
+{
+	/* Adress of boot parameters */
+	gd->bd->bi_boot_params = PHYS_SDRAM_1 + 0x100;
+
+	return 0;
+}
+
+#ifdef	CONFIG_CMD_MMC
+
+int board_mmc_init(bd_t *bis)
+{
+	return mxsmmc_initialize(bis, 0, NULL);
+}
+#endif
+
+#ifdef	CONFIG_CMD_NET
+
+int board_eth_init(bd_t *bis)
+{
+	struct mxs_clkctrl_regs *clkctrl_regs =
+		(struct mxs_clkctrl_regs *)MXS_CLKCTRL_BASE;
+	struct eth_device *dev;
+	int ret;
+
+	ret = cpu_eth_init(bis);
+
+	/* ccardimx28 uses ENET_CLK PAD to drive FEC clock */
+	writel(CLKCTRL_ENET_TIME_SEL_RMII_CLK | CLKCTRL_ENET_CLK_OUT_EN,
+					&clkctrl_regs->hw_clkctrl_enet);
+
+	/* Reset FEC PHYs */
+	gpio_direction_output(MX28_PAD_PWM4__GPIO_3_29, 0);
+	udelay(10300);  // from datasheet: tvr + tsr
+	gpio_set_value(MX28_PAD_PWM4__GPIO_3_29, 1);
+	/* Ensure recommended delay of 100us in PHY datasheet is met before
+	 * accessing the MDIO interface */
+	udelay(100);
+
+	ret = fecmxc_initialize_multi(bis, 0, 0, MXS_ENET0_BASE);
+	if (ret) {
+		puts("FEC MXS: Unable to init FEC0\n");
+		return ret;
+	}
+
+	ret = fecmxc_initialize_multi(bis, 1, 3, MXS_ENET1_BASE);
+	if (ret) {
+		puts("FEC MXS: Unable to init FEC1\n");
+		return ret;
+	}
+
+	dev = eth_get_dev_by_name("FEC0");
+	if (!dev) {
+		puts("FEC MXS: Unable to get FEC0 device entry\n");
+		return -EINVAL;
+	}
+
+#ifndef CONFIG_FEC1_INIT_ONLY_MAC
+	dev = eth_get_dev_by_name("FEC1");
+	if (!dev) {
+		puts("FEC MXS: Unable to get FEC1 device entry\n");
+		return -EINVAL;
+	}
+#endif
+
+	return ret;
+}
+
+void mx28_adjust_mac(int dev_id, unsigned char *mac)
+{
+	nv_critical_t *pNvram;
+
+	/* Get MAC from Digi NVRAM */
+	if (!NvCriticalGet(&pNvram))
+		return;
+
+	if (0 == dev_id) {
+		/* 1st wired MAC (ENET0) */
+		memcpy(mac, &pNvram->s.p.xID.axMAC[0], 6);
+	}
+	else if (1 == dev_id) {
+		/* 2nd wired MAC (ENET1) corresponds to ethaddr3
+		 * because ethaddr2 is for the wireless.
+		 * ethaddr3 is a field out of xID which originally
+		 * only had space for two interfaces (wired and wireless)
+		 */
+		memcpy(mac, &pNvram->s.p.eth1addr, 6);
+	}
+
+	return;
+}
+
+#endif
