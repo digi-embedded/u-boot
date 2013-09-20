40705504d6d0c6b9bad845e47bdb537347b5b229
Alex Gonzalez <alex.gonzalez@digi.com>
2013-09-20 12:22:59 +0200

    Add support for the ccimx6js board.
    This is based on a i.mx6 dual/quad SoC, and the initial support
    is copied from the imx6q-sabresd reference BSP.
    
    https://jira.digi.com/browse/DEL-794
    
    Signed-off-by: Alex Gonzalez <alex.gonzalez@digi.com>

---
+++ include/configs/ccimx6js.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2012-2013 Freescale Semiconductor, Inc.
+ * Copyright (C) 2013 Digi International, Inc.
+ *
+ * Configuration settings for the Freescale i.MX6Q SabreSD board.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.         See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __CCIMX6JS_CONFIG_H
+#define __CCIMX6JS_CONFIG_H
+
+#define CONFIG_MACH_TYPE	3980
+#define CONFIG_MXC_UART_BASE	UART1_BASE
+#define CONFIG_CONSOLE_DEV		"ttymxc0"
+#define CONFIG_MMCROOT			"/dev/mmcblk0p2"
+
+#include "ccimx6js_common.h"
+#include <asm/imx-common/gpio.h>
+
+#define CONFIG_SYS_FSL_USDHC_NUM	3
+#define CONFIG_SYS_MMC_ENV_DEV		1	/* SDHC3 */
+#define CONFIG_SYS_MMC_ENV_PART		0	/* user partition */
+
+#ifdef CONFIG_SYS_USE_SPINOR
+#define CONFIG_SF_DEFAULT_CS   (0|(IMX_GPIO_NR(4, 9)<<8))
+#endif
+
+#endif                         /* __CCIMX6JS_CONFIG_H */
