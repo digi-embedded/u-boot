ed6eb97c562fe29150f54819295548d16b9dd5b5
Hector Palacios <hector.palacios@digi.com>
2014-01-15 12:49:59 +0100

    ccimx6adpt: remove Freescale's own PMIC setup
    This board uses a Dialog DA9063 PMIC.
    The PMIC configuration from SabreSD board is not valid.
    
    Signed-off-by: Hector Palacios <hector.palacios@digi.com>

--- include/configs/ccimx6_common.h
+++ include/configs/ccimx6_common.h
@@ -350,6 +350,6 @@
 #define CONFIG_I2C_MXC          1
 #define CONFIG_SYS_I2C_BASE             I2C2_BASE_ADDR
 #define CONFIG_SYS_I2C_SPEED            100000
-#define CONFIG_SYS_I2C_SLAVE            0x8
+#define CONFIG_SYS_I2C_SLAVE            0	/* unused */
 
 #endif                         /* __CCIMX6_COMMON_CONFIG_H */
