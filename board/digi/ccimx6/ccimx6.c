197a2d43b8412e5c4231aed44e93db16c4cf90df
Alex Gonzalez <alex.gonzalez@digi.com>
2016-05-09 11:31:20 +0200

    mx6sbc: Add passphrase console enable.
    This allows to enable the console with a passphrase.
    
    https://jira.digi.com/browse/DEL-2474
    
    Signed-off-by: Alex Gonzalez <alex.gonzalez@digi.com>

--- board/digi/ccimx6/ccimx6.c
+++ board/digi/ccimx6/ccimx6.c
@@ -36,6 +36,9 @@
 #endif
 #include <linux/ctype.h>
 #include <mmc.h>
+#include <malloc.h>
+#include <watchdog.h>
+#include <u-boot/sha256.h>
 #include <fsl_esdhc.h>
 #include <fuse.h>
 #include <otf_update.h>
@@ -1210,6 +1213,146 @@ void pmic_bucks_synch_mode(void)
 	}
 }
 
+#define STR_HEX_CHUNK 			8
+/*
+ * Convert string with hexadecimal characters into a hex number
+ * @in: Pointer to input string
+ * @out Pointer to output number array
+ * @len Number of elements in the output array
+*/
+static void strtohex(char * in, unsigned long * out, int len)
+{
+	char tmp[] = "ffffffff";
+	int i, j;
+
+	for(i = 0, j = 0; j < len; i += STR_HEX_CHUNK, j++) {
+		strncpy(tmp, &in[i], STR_HEX_CHUNK);
+		out[j] = cpu_to_be32(simple_strtol(tmp, NULL, 16));
+	}
+}
+
+#ifdef CONFIG_CONSOLE_ENABLE_PASSPHRASE
+#define INACTIVITY_TIMEOUT 		2
+/*
+ * Grab a passphrase from the console input
+ * @secs: Inactivity timeout in seconds
+ * @buff: Pointer to passphrase output buffer
+ * @len: Length of output buffer
+ *
+ * Returns zero on success and a negative number on error.
+ */
+static int console_get_passphrase(int secs, char * buff, int len)
+{
+	char c;
+	uint64_t end_tick, timeout;
+	int i;
+
+	/* Set a global timeout to avoid DoS attacks */
+	end_tick = get_ticks() + (uint64_t)(secs) * get_tbclk();
+
+	/* Set an inactivity timeout */
+	timeout = get_ticks() + INACTIVITY_TIMEOUT * get_tbclk();
+
+	*buff = '\0';
+	for (i = 0; i < len;) {
+		/* Check timeouts */
+		uint64_t tks = get_ticks();
+		if ((tks > end_tick) || (tks > timeout)) {
+			*buff = '\0';
+			return -ETIME;
+		}
+
+		/* Do not trigger watchdog while typing passphrase */
+		WATCHDOG_RESET();
+
+		if (tstc()) {
+			c = getc();
+			i++;
+		} else
+			continue;
+
+		switch (c) {
+			/* Enter */
+			case '\r':
+			case '\n':
+				*buff = '\0';
+				return 0;
+			/* nul */
+			case '\0':
+				continue;
+			/* Ctrl-c */
+			case 0x03:
+				*buff = '\0';
+				return -EINVAL;
+			default:
+				*buff++ = c;
+				/* Restart inactivity timeout */
+				timeout = get_ticks() + INACTIVITY_TIMEOUT *
+					get_tbclk();
+		}
+	}
+
+	return -EINVAL;
+}
+
+#define SHA256_HASH_LEN 		32
+#define PASSPHRASE_SECS_TIMEOUT		10
+#define MAX_PP_LEN 			64
+/*
+ * Returns zero (success) to enable the console, or a non zero otherwise.
+ *
+ * A sha256 hash is 256 bits (32 bytes) long, and is represented as
+ * a 64 digits hex number.
+ */
+static int console_enable_passphrase(void)
+{
+	char * pp = NULL;
+	unsigned char * sha256_pp = NULL;
+	unsigned long * pp_hash = NULL;
+	int ret = -EINVAL;
+
+	pp = malloc(MAX_PP_LEN + 1);
+	if (!pp) {
+		debug("Not enough memory for passphrase\n");
+		return -ENOMEM;
+	}
+
+	ret = console_get_passphrase(PASSPHRASE_SECS_TIMEOUT,
+				     pp, MAX_PP_LEN);
+	if (ret)
+		goto pp_error;
+
+	sha256_pp = malloc(SHA256_HASH_LEN);
+	if (!sha256_pp) {
+		debug("Not enough memory for passphrase\n");
+		ret = -ENOMEM;
+		goto pp_error;
+	}
+
+	sha256_csum_wd((const unsigned char *)pp, strlen(pp),
+			sha256_pp, CHUNKSZ_SHA256);
+
+	pp_hash = malloc(SHA256_HASH_LEN);
+	if (!pp_hash) {
+		debug("Not enough memory for passphrase\n");
+		ret = -ENOMEM;
+		goto pp_hash_error;
+	}
+
+	memset(pp_hash, 0, SHA256_HASH_LEN);
+	strtohex(CONFIG_CONSOLE_ENABLE_PASSPHRASE_KEY, pp_hash,
+			SHA256_HASH_LEN/sizeof(unsigned long));
+	ret = memcmp(sha256_pp, pp_hash, SHA256_HASH_LEN);
+
+	free(pp_hash);
+pp_hash_error:
+	free(sha256_pp);
+pp_error:
+	free(pp);
+	return ret;
+}
+#endif
+
 int ccimx6_late_init(void)
 {
 #ifdef CONFIG_CMD_MMC
@@ -1280,6 +1423,15 @@ int ccimx6_late_init(void)
 			run_command(recoverycmd, 0);
 	}
 #endif
+
+#ifdef CONFIG_CONSOLE_ENABLE_PASSPHRASE
+	gd->flags &= ~GD_FLG_DISABLE_CONSOLE_INPUT;
+	if (!console_enable_passphrase())
+		gd->flags &= ~(GD_FLG_DISABLE_CONSOLE | GD_FLG_SILENT);
+	else
+		gd->flags |= GD_FLG_DISABLE_CONSOLE_INPUT;
+#endif
+
 	return ccimx6_fixup();
 }
 
