fa3c2c9da9a0aba6e1f3622f518c527cacc930a4
Hector Palacios <hector.palacios@digi.com>
2016-12-26 10:44:03 +0100

    ccimx6ulsbc: add PHY reset support for ENET2
    The ENET2 PHY reset is driven by i.MX6UL GPIO5_6.
    With the addition of this support, the selection of the active
    Ethernet controller can easily be done by defining
    CONFIG_FEC_ENET_DEV on the platform header file.
    
    Signed-off-by: Hector Palacios <hector.palacios@digi.com>
    
    https://jira.digi.com/browse/DUB-710

--- board/digi/ccimx6ulsbc/ccimx6ulsbc.c
+++ board/digi/ccimx6ulsbc/ccimx6ulsbc.c
@@ -214,9 +214,9 @@ int board_mmc_init(bd_t *bis)
 #endif
 
 #ifdef CONFIG_FEC_MXC
-void reset_phy()
+void reset_phy(void)
 {
-	int reset = (1 << 7);	/* MCA_IO7 is connected to PHY reset */
+	int reset;
 
 	/*
 	 * The reset line must be held low for a minimum of 100usec and cannot
@@ -224,14 +224,25 @@ void reset_phy()
 	 * reached 80% of the operating voltage. At this point of the code
 	 * we can assume the second premise is already accomplished.
 	 */
-
-	/* Configure as output */
-	mca_update_bits(MCA_CC6UL_GPIO_DIR_0, reset, reset);
-	/* Assert PHY reset (low) */
-	mca_update_bits(MCA_CC6UL_GPIO_DATA_0, reset, 0);
-	udelay(100);
-	/* Deassert PHY reset (high) */
-	mca_update_bits(MCA_CC6UL_GPIO_DATA_0, reset, reset);
+	if (CONFIG_FEC_ENET_DEV == 0) {
+		/* MCA_IO7 is connected to PHY reset */
+		reset = (1 << 7);
+		/* Configure as output */
+		mca_update_bits(MCA_CC6UL_GPIO_DIR_0, reset, reset);
+		/* Assert PHY reset (low) */
+		mca_update_bits(MCA_CC6UL_GPIO_DATA_0, reset, 0);
+		udelay(100);
+		/* Deassert PHY reset (high) */
+		mca_update_bits(MCA_CC6UL_GPIO_DATA_0, reset, reset);
+	} else if (CONFIG_FEC_ENET_DEV == 1) {
+		/* CPU GPIO5_6 is connected to PHY reset */
+		reset = IMX_GPIO_NR(5, 6);
+		/* Assert PHY reset (low) */
+		gpio_direction_output(reset, 0);
+		udelay(100);
+		/* Deassert PHY reset (high) */
+		gpio_set_value(reset, 1);
+	}
 }
 
 int board_eth_init(bd_t *bis)
