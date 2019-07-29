a27bb329bbf7a475c92f2782c7d6664533a6e5eb
Patrick Delaunay <patrick.delaunay@st.com>
2019-07-29 16:46:29 +0200

    dts: stm32mp1: alignment with v5.10-stm32mp-r1
    Device tree alignment with Linux kernel v5.10-stm32mp-r1
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@st.com>
    Change-Id: Iff062ef9f417c3b6d6ef0ce0c7ada8f4f7ef334c
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/u-boot/+/192682
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/u-boot/+/194405
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/u-boot/+/194422

--- board/st/stm32mp1/stm32mp1.c
+++ board/st/stm32mp1/stm32mp1.c
@@ -602,7 +602,8 @@ error:
 static bool board_is_dk2(void)
 {
 	if (CONFIG_IS_ENABLED(TARGET_ST_STM32MP15x) &&
-	    of_machine_is_compatible("st,stm32mp157c-dk2"))
+	    (of_machine_is_compatible("st,stm32mp157c-dk2") ||
+	     of_machine_is_compatible("st,stm32mp157f-dk2")))
 		return true;
 
 	return false;
