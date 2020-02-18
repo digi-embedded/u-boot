64ae7eb08b484ec0735ac39cf573f1481b19cd90
Mike Engel <Mike.Engel@digi.com>
2020-02-18 18:24:16 +0100

    imx6: caam: introduce key_modifier as a parameter for data blob operations
    This is required in order to use the CAAM blob encapsulation and
    decapsulation operations with a secure unique key modifier, instead of the
    default one.
    
    When using the caam U-Boot command a null key modifier will be used.
    
    For U-Boot environment encryption the md5 hash of the OTP words containing
    the digi HWID (unique per module) is used as key modifier.
    
    https://jira.digi.com/browse/DUB-652
    
    Imported from commit f91786b09a7918f58d9e3a34bf6a17590db65da3
    
    Signed-off-by: Mike Engel <Mike.Engel@digi.com>
    
    https://jira.digi.com/browse/DUB-925
    (cherry picked from commit 373fe58e05341fb3ff89b6e1dee4fed0ad3367dc)

--- include/fsl_caam.h
+++ include/fsl_caam.h
@@ -45,6 +45,7 @@
 #define ERROR_IN_PAGE_ALLOC (1)
 #endif
 
+#define KEY_MODIFER_SIZE 16
 
 ////////////////////////////////////////////////////////////////////////////////
 //! @brief Enable and initialize the CAAM driver.
@@ -66,7 +67,7 @@ void imx_sec_init(void);
 //! @return SUCCESS
 //! @return ERROR_XXX
 ////////////////////////////////////////////////////////////////////////////////
-uint32_t caam_gen_blob(uint32_t plain_data_addr, uint32_t blob_addr, uint32_t size);
+uint32_t caam_gen_blob(uint32_t plain_data_addr, uint32_t blob_addr, void *key_modifier, uint32_t size);
 
 ////////////////////////////////////////////////////////////////////////////////
 //! @brief Decapsulate a blob of a secure key.
@@ -76,7 +77,7 @@ uint32_t caam_gen_blob(uint32_t plain_data_addr, uint32_t blob_addr, uint32_t si
 //! @return SUCCESS
 //! @return ERROR_XXX
 ////////////////////////////////////////////////////////////////////////////////
-uint32_t caam_decap_blob(uint32_t plain_text, uint32_t blob_addr, uint32_t size);
+uint32_t caam_decap_blob(uint32_t plain_text, uint32_t blob_addr, void * key_modifier, uint32_t size);
 uint32_t caam_hwrng(uint8_t *output_ptr, uint32_t output_len);
 
 #endif /* __CAAM_H__ */
