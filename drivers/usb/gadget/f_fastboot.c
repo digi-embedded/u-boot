134784b9d2a6ba6dd0fa660feb7208032307d2ee
Javier Viguera <javier.viguera@digi.com>
2020-05-22 18:59:13 +0200

    fastboot: fix bootloader mmc offset calculation
    This fixes a regression introduced in commit 3ab65fa90762. Before that
    commit the function returned an absolute MMC offset for the bootloader
    to be flashed to, but that commit changed it to a block number. So the
    semantics of the returned value changed and thus the consumers of this
    function are using a wrong value.
    
    This commit changes it back to an absolute offset, while still using the
    common code we use in the 'update' command.
    
    https://jira.digi.com/browse/DUB-958
    
    Signed-off-by: Javier Viguera <javier.viguera@digi.com>

--- drivers/usb/gadget/f_fastboot.c
+++ drivers/usb/gadget/f_fastboot.c
@@ -805,12 +805,12 @@ static ulong bootloader_mmc_offset(void)
 							  mmc_dev_index);
 	if (NULL == mmc_dev) {
 		debug("Cannot determine sys storage device\n");
-		return (EMMC_BOOT_PART_OFFSET / mmc_dev->blksz);
+		return EMMC_BOOT_PART_OFFSET;
 	}
 
 	calculate_uboot_update_settings(mmc_dev, &info);
 
-	return info.start;
+	return info.start * mmc_dev->blksz;
 }
 
 #if defined(CONFIG_FASTBOOT_STORAGE_MMC) || defined(CONFIG_FASTBOOT_STORAGE_SATA)
