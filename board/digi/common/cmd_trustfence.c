d0eafbe6d3b4229099e66ccd3a5488d57a8e8300
Hector Palacios <hector.palacios@digi.com>
2020-07-16 09:18:10 +0200

    trustfence: make fdt_fixup_trustfence a generally available function
    This function was only available under the ifdef CONFIG_ENV_AES_CAAM_KEY,
    but this condition must now be limited to the encrypted environment case.
    
    Signed-off-by: Hector Palacios <hector.palacios@digi.com>
    
    https://jira.digi.com/browse/DEL-7185
    (cherry picked from commit 4c8524c2535ae104f727fc6dfcb7d29fc408c9c8)
    (cherry picked from commit d81cea7a06de23c4ce846d070166638af26672e2)
    (cherry picked from commit c9877e96ae048cad23f40ca08397631b4ff1803f)

--- board/digi/common/cmd_trustfence.c
+++ board/digi/common/cmd_trustfence.c
@@ -22,6 +22,7 @@
 
 #include <common.h>
 #include <command.h>
+#include <fdt_support.h>
 #include <fsl_sec.h>
 #include <fuse.h>
 #include <asm/imx-common/hab.h>
@@ -558,9 +559,6 @@ uint8_t *env_aes_cbc_get_key(void)
 }
 #endif
 
-#ifdef CONFIG_ENV_AES_CAAM_KEY
-#include <fdt_support.h>
-
 void fdt_fixup_trustfence(void *fdt) {
 	/* Environment encryption is not enabled on open devices */
 	if (!is_hab_enabled()) {
@@ -568,10 +566,11 @@ void fdt_fixup_trustfence(void *fdt) {
 		return;
 	}
 
+#ifdef CONFIG_ENV_AES_CAAM_KEY
 	do_fixup_by_path(fdt, "/", "digi,uboot-env,encrypted", NULL, 0, 1);
+#endif
 	do_fixup_by_path(fdt, "/", "digi,tf-closed", NULL, 0, 1);
 }
-#endif
 
 static int do_trustfence(cmd_tbl_t *cmdtp, int flag, int argc, char *const argv[])
 {
