3daf7713c940ae97d0ca8eb66482925e482d9e9f
Gonzalo Ruiz <Gonzalo.Ruiz@digi.com>
2020-09-17 10:15:04 +0200

    trustfence: adapt console_enable_gpio to DM_GPIO
    Adapt console_enable_gpio implementation to the GPIO driver model.
    Update boards implementation to pass the GPIO name instead of the number.
    
    Signed-off-by: Gonzalo Ruiz <Gonzalo.Ruiz@digi.com>
    (cherry picked from commit f9ab230acbb8d76f5836dec78b5ea79802584842)

--- cmd/trustfence.c
+++ cmd/trustfence.c
@@ -282,15 +282,30 @@ static void board_print_trustfence_jtag_key(u32 *sjc)
 }
 
 #ifdef CONFIG_CONSOLE_ENABLE_GPIO
-int console_enable_gpio(int gpio)
+/*
+ * Enable console when selected GPIO level is HIGH
+ * @name: Name of the GPIO to read
+ *
+ * Returns the GPIO level on success and 0 on error.
+ */
+int console_enable_gpio(const char *name)
 {
+	struct gpio_desc desc;
 	int ret = 0;
 
-	if (gpio_request(gpio, "console_enable") == 0) {
-		ret = gpio_get_value(gpio);
-		gpio_free(gpio);
-	}
+	if (dm_gpio_lookup_name(name, &desc))
+		goto error;
 
+	if (dm_gpio_request(&desc, "Console enable"))
+		goto error;
+
+	if (dm_gpio_set_dir_flags(&desc, GPIOD_IS_IN))
+		goto errfree;
+
+	ret = dm_gpio_get_value(&desc);
+errfree:
+	dm_gpio_free(NULL, &desc);
+error:
 	return ret;
 }
 #endif
