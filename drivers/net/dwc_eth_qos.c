ee99077acf117cb9f57dc61a9e4338f8285ef3b6
Christophe Roullier <christophe.roullier@st.com>
2020-10-01 10:51:34 +0200

    net: dwc_eth_qos: add support of stm32mp13 platform
    Add compatible "st,stm32mp13-dwmac" to manage STM32MP13 boards
    
    Change-Id: Ie35cc73a27da1729e9cf64b04ba4505a02a38865
    Signed-off-by: Christophe Roullier <christophe.roullier@st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/u-boot/+/179668
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: CIBUILD <smet-aci-builds@lists.codex.cro.st.com>
    Reviewed-by: Patrick DELAUNAY <patrick.delaunay@st.com>

--- drivers/net/dwc_eth_qos.c
+++ drivers/net/dwc_eth_qos.c
@@ -1989,6 +1989,17 @@ static const struct eqos_config __maybe_unused eqos_stm32_config = {
 	.ops = &eqos_stm32_ops
 };
 
+static const struct eqos_config eqos_stm32mp13_config = {
+	.reg_access_always_ok = false,
+	.mdio_wait = 10000,
+	.swr_wait = 50,
+	.config_mac = EQOS_MAC_RXQ_CTRL0_RXQ0EN_ENABLED_DCB,
+	.config_mac_mdio = EQOS_MAC_MDIO_ADDRESS_CR_250_300,
+	.axi_bus_width = EQOS_AXI_WIDTH_32,
+	.interface = eqos_get_interface_stm32,
+	.ops = &eqos_stm32_ops
+};
+
 static struct eqos_ops eqos_imx_ops = {
 	.eqos_inval_desc = eqos_inval_desc_generic,
 	.eqos_flush_desc = eqos_flush_desc_generic,
@@ -2028,6 +2039,10 @@ static const struct udevice_id eqos_ids[] = {
 	{
 		.compatible = "st,stm32mp1-dwmac",
 		.data = (ulong)&eqos_stm32_config
+	},
+	{
+		.compatible = "st,stm32mp13-dwmac",
+		.data = (ulong)&eqos_stm32mp13_config
 	},
 #endif
 #if IS_ENABLED(CONFIG_DWC_ETH_QOS_IMX)
