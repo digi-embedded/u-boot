9bac53ceb9b5a64298dafa72edc03fc773f1d824
Patrick Delaunay <patrick.delaunay@foss.st.com>
2020-12-14 15:07:04 +0100

    stm32mp: stm32prog: Add CONFIG_CMD_STM32PROG_SERIAL and _USB
    Add CONFIG_CMD_STM32PROG_SERIAL and CONFIG_CMD_STM32PROG_USB to
    independantly select the support of UART or USBB communication for
    STM32CubeProgrammer.
    
    For serial boot over UART, user can deactivate CONFIG_CMD_STM32PROG_SERIAL
    to use U-Boot console of binary loaded by UART (for board bring-up for
    example).
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Change-Id: I60352ed0b0f59ee019230f63aaa3c45fd4438531
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/u-boot/+/188437
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: Patrick DELAUNAY <patrick.delaunay@st.com>
    Tested-by: Patrick DELAUNAY <patrick.delaunay@st.com>

--- arch/arm/mach-stm32mp/cpu.c
+++ arch/arm/mach-stm32mp/cpu.c
@@ -263,7 +263,8 @@ int arch_cpu_init(void)
 
 	boot_mode = get_bootmode();
 
-	if ((boot_mode & TAMP_BOOT_DEVICE_MASK) == BOOT_SERIAL_UART)
+	if (IS_ENABLED(CONFIG_CMD_STM32PROG_SERIAL) &&
+	    (boot_mode & TAMP_BOOT_DEVICE_MASK) == BOOT_SERIAL_UART)
 		gd->flags |= GD_FLG_SILENT | GD_FLG_DISABLE_CONSOLE;
 #if defined(CONFIG_DEBUG_UART) && \
 	!defined(CONFIG_TFABOOT) && \
@@ -483,7 +484,8 @@ static void setup_boot_mode(void)
 		env_set("boot_instance", cmd);
 
 		/* restore console on uart when not used */
-		if (gd->cur_serial_dev != dev) {
+		if (IS_ENABLED(CONFIG_CMD_STM32PROG_SERIAL) &&
+		    (gd->cur_serial_dev != dev)) {
 			gd->flags &= ~(GD_FLG_SILENT |
 				       GD_FLG_DISABLE_CONSOLE);
 			printf("serial boot with console enabled!\n");
