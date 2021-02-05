d015f8127f09ecb6b3c54e8a2f6defaadea474fc
Patrick Delaunay <patrick.delaunay@foss.st.com>
2021-02-05 13:53:32 +0100

    stm32mp: update MMU config before the relocation
    Mark the top of ram, used for relocated U-Boot as a normal memory
    (cacheable and executable) to avoid permission access issue when
    U-Boot jumps to this relocated code.
    
    When MMU is activated in pre-reloc stage; only the beginning of
    DDR is marked executable.
    
    This patch avoids access issue when DACR is correctly managed.
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Change-Id: Id05ed63c4c424d0307d757026ab2f22621b5481b
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/u-boot/+/201214
    Reviewed-by: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: CIBUILD <MDG-smet-aci-builds@list.st.com>

--- arch/arm/mach-stm32mp/dram_init.c
+++ arch/arm/mach-stm32mp/dram_init.c
@@ -10,6 +10,7 @@
 #include <lmb.h>
 #include <log.h>
 #include <ram.h>
+#include <asm/system.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -38,6 +39,7 @@ int dram_init(void)
 
 ulong board_get_usable_ram_top(ulong total_size)
 {
+	phys_size_t size;
 	phys_addr_t reg;
 	struct lmb lmb;
 
@@ -45,10 +47,13 @@ ulong board_get_usable_ram_top(ulong total_size)
 	lmb_init(&lmb);
 	lmb_add(&lmb, gd->ram_base, gd->ram_size);
 	boot_fdt_add_mem_rsv_regions(&lmb, (void *)gd->fdt_blob);
-	reg = lmb_alloc(&lmb, CONFIG_SYS_MALLOC_LEN + total_size, SZ_4K);
+	size = ALIGN(CONFIG_SYS_MALLOC_LEN + total_size, MMU_SECTION_SIZE),
+	reg = lmb_alloc(&lmb, size, MMU_SECTION_SIZE);
 
-	if (reg)
-		return ALIGN(reg + CONFIG_SYS_MALLOC_LEN + total_size, SZ_4K);
+	if (!reg)
+		reg = gd->ram_top - size;
 
-	return gd->ram_top;
+	mmu_set_region_dcache_behaviour(reg, size, DCACHE_DEFAULT_OPTION);
+
+	return reg + size;
 }
