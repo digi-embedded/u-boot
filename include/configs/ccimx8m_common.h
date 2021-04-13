5446e51a7b923df13bdf9e658780109b92cbf9b3
Mike Engel <Mike.Engel@digi.com>
2021-04-13 11:04:28 +0200

    configs: add Dualboot partitions and CONFIG_DIGI_DUALBOOT
    Signed-off-by: Mike Engel <Mike.Engel@digi.com>
    
    https://jira.digi.com/browse/DEL-7486

--- include/configs/ccimx8m_common.h
+++ include/configs/ccimx8m_common.h
@@ -243,13 +243,47 @@
 	"part9_uuid=dc83dea8-c467-45dc-84eb-5e913daec17e\0" \
 	"part10_uuid=df0dba76-d5e0-11e8-9f8b-f2801f1b9fd1\0"
 
+#ifdef CONFIG_DIGI_DUALBOOT
+#define LINUX_SYSTEM_BOOT_PARTITION \
+        "name=linux_a,size=64MiB,uuid=${part1_uuid};" \
+        "name=linux_b,size=64MiB,uuid=${part2_uuid};"
+
+#define LINUX_4GB_ROOTFS_PARTITION_TABLE \
+        "name=rootfs_a,size=1536MiB,uuid=${part3_uuid};" \
+        "name=rootfs_b,size=1536MiB,uuid=${part4_uuid};"
+
+#define LINUX_8GB_ROOTFS_PARTITION_TABLE \
+	"name=rootfs_a,size=3GiB,uuid=${part3_uuid};" \
+        "name=rootfs_b,size=3GiB,uuid=${part4_uuid};"
+
+#define LINUX_16GB_ROOTFS_PARTITION_TABLE \
+	"name=rootfs_a,size=7GiB,uuid=${part3_uuid};" \
+        "name=rootfs_b,size=7GiB,uuid=${part4_uuid};"
+
+#else
+#define LINUX_SYSTEM_BOOT_PARTITION \
+        "name=linux,size=64MiB,uuid=${part1_uuid};" \
+        "name=recovery,size=64MiB,uuid=${part2_uuid};"
+
+#define LINUX_4GB_ROOTFS_PARTITION_TABLE \
+        "name=rootfs,size=1536MiB,uuid=${part3_uuid};" \
+        "name=update,size=1536MiB,uuid=${part4_uuid};"
+
+#define LINUX_8GB_ROOTFS_PARTITION_TABLE \
+        "name=rootfs,size=3GiB,uuid=${part3_uuid};" \
+        "name=update,size=3GiB,uuid=${part4_uuid};"
+
+#define LINUX_16GB_ROOTFS_PARTITION_TABLE \
+        "name=rootfs,size=7GiB,uuid=${part3_uuid};" \
+        "name=update,size=7GiB,uuid=${part4_uuid};"
+
+#endif
+
 #define LINUX_4GB_PARTITION_TABLE \
 	"\"uuid_disk=${uuid_disk};" \
 	"start=2MiB," \
-	"name=linux,size=64MiB,uuid=${part1_uuid};" \
-	"name=recovery,size=64MiB,uuid=${part2_uuid};" \
-	"name=rootfs,size=1536MiB,uuid=${part3_uuid};" \
-	"name=update,size=1536MiB,uuid=${part4_uuid};" \
+	LINUX_SYSTEM_BOOT_PARTITION \
+	LINUX_4GB_ROOTFS_PARTITION_TABLE \
 	"name=safe,size=16MiB,uuid=${part5_uuid};" \
 	"name=safe2,size=16MiB,uuid=${part6_uuid};" \
 	"name=data,size=-,uuid=${part7_uuid};" \
@@ -258,10 +292,8 @@
 #define LINUX_8GB_PARTITION_TABLE \
 	"\"uuid_disk=${uuid_disk};" \
 	"start=2MiB," \
-	"name=linux,size=64MiB,uuid=${part1_uuid};" \
-	"name=recovery,size=64MiB,uuid=${part2_uuid};" \
-	"name=rootfs,size=3GiB,uuid=${part3_uuid};" \
-	"name=update,size=3GiB,uuid=${part4_uuid};" \
+	LINUX_SYSTEM_BOOT_PARTITION \
+	LINUX_8GB_ROOTFS_PARTITION_TABLE \
 	"name=safe,size=16MiB,uuid=${part5_uuid};" \
 	"name=safe2,size=16MiB,uuid=${part6_uuid};" \
 	"name=data,size=-,uuid=${part7_uuid};" \
@@ -270,10 +302,8 @@
 #define LINUX_16GB_PARTITION_TABLE \
 	"\"uuid_disk=${uuid_disk};" \
 	"start=2MiB," \
-	"name=linux,size=64MiB,uuid=${part1_uuid};" \
-	"name=recovery,size=64MiB,uuid=${part2_uuid};" \
-	"name=rootfs,size=7GiB,uuid=${part3_uuid};" \
-	"name=update,size=7GiB,uuid=${part4_uuid};" \
+	LINUX_SYSTEM_BOOT_PARTITION \
+	LINUX_16GB_ROOTFS_PARTITION_TABLE \
 	"name=safe,size=16MiB,uuid=${part5_uuid};" \
 	"name=safe2,size=16MiB,uuid=${part6_uuid};" \
 	"name=data,size=-,uuid=${part7_uuid};" \
