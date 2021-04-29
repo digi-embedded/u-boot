1181d23abe20c626622a618fb9793ccab1a05a46
Hector Bujanda <hector.bujanda@digi.com>
2021-04-30 01:49:35 +0200

    mca: add support for GPIO-controlled watchdog
    Current features:
       - Allow to configure one or several GPIOs to kick the watchdog timer.
       - Configurable hw_algo: toggle, level_high and level_low.
       - Configurable timeout value (1-255 seconds).
       - way-out, no-way-out configuration option.
    
    Device tree entry example:
    mca_wdt_uboot: mca_wdt {
    	compatible = "digi,mca-wdt";
    
    	soft-rfsh {
    		digi,full-reset;
    		digi,no-way-out;
    	};
    
    	gpio-rfsh {
    		gpio@0 {
    			digi,full-reset;
    			digi,no-way-out;
    			/* mode; one of: toggle, level-high, level-low */
    			digi,mode = "level-high";
    			digi,mca-io-number = <10>;
    			digi,timeout-sec = <2>;
    		};
    
    		gpio@1 {
    			digi,full-reset;
    			digi,no-way-out;
    			/* mode; one of: toggle, level-high, level-low */
    			digi,mode = "toggle";
    			digi,mca-io-number = <16>;
    			digi,timeout-sec = <1>;
    		};
    	};
    };
    
    https://jira.digi.com/browse/DEL-7494
    
    Signed-off-by: Hector Bujanda <hector.bujanda@digi.com>

--- drivers/watchdog/mca_wdt.c
+++ drivers/watchdog/mca_wdt.c
@@ -21,9 +21,34 @@
 				 MCA_WDT_REFRESH_0 + 1)
 #define WDT_REFRESH_PATTERN	"WDTP"
 
-struct mca_wdt_priv {
+#define MCA_NUM_GPIO_WDG		4
+#define MCA_GPIO_WDG_MIN_TOUT_SEC	1
+#define MCA_GPIO_WDG_MAX_TOUT_SEC	255
+
+struct mca_wdt_soft {
+	bool enabled;
 	bool full_reset;
 	bool no_way_out;
+};
+
+typedef enum mca_wdg_gpio_mode {
+	MCA_GPIO_WD_TOGGLE,
+	MCA_GPIO_WD_LEVEL_HIGH,
+	MCA_GPIO_WD_LEVEL_LOW,
+} mca_wdg_gpio_mode_t;
+
+struct mca_wdt_gpio {
+	bool full_reset;
+	bool no_way_out;
+	u32  mca_io_number;
+	u32  timeout_sec;
+	mca_wdg_gpio_mode_t mode;
+};
+
+struct mca_wdt_priv {
+	struct mca_wdt_soft wdt_soft;
+	u8 wdt_gpio_num;
+	struct mca_wdt_gpio wdt_gpio[MCA_NUM_GPIO_WDG];
 };
 
 static int mca_wdt_reset(struct udevice *dev)
@@ -38,11 +63,20 @@ static int mca_wdt_reset(struct udevice *dev)
 
 static int mca_wdt_stop(struct udevice *dev)
 {
+	struct mca_wdt_priv *priv = dev_get_priv(dev);
 	int ret = 0;
+	u8 i;
 
 	ret = mca_update_bits(MCA_WDT_CONTROL, MCA_WDT_ENABLE, 0);
 	if (ret)
-		printf("Could not stop watchdog (%d)\n", ret);
+		printf("Couldn't stop watchdog (%d)\n", ret);
+
+	for (i = 0 ; i < priv->wdt_gpio_num ; i++) {
+		ret = mca_update_bits(MCA_GPIO_WDT0_CONTROL + 0x10*i,
+				      MCA_WDT_ENABLE, 0);
+		if (ret)
+			printf("Couldn't stop gpio wdt %d (%d)\n", i, ret);
+	}
 
 	return ret;
 }
@@ -50,33 +84,66 @@ static int mca_wdt_stop(struct udevice *dev)
 static int mca_wdt_start(struct udevice *dev, u64 timeout, ulong flags)
 {
 	struct mca_wdt_priv *priv = dev_get_priv(dev);
-	u16 mca_timeout;
 	u8 control;
 	int ret = 0;
+	u8 i;
 
-	mca_timeout = timeout / 1000;
+	if (priv->wdt_soft.enabled) {
+		const u16 mca_timeout = timeout / 1000;
 
-	/* Set timeout */
-	ret = mca_write_reg(MCA_WDT_TIMEOUT, mca_timeout);
-	if (ret) {
-		printf("Could not set watchdog timeout (%d)\n", ret);
-		goto err;
+		/* Set timeout */
+		ret = mca_write_reg(MCA_WDT_TIMEOUT, mca_timeout);
+		if (ret) {
+			printf("Couldn't set watchdog timeout (%d)\n", ret);
+			goto err;
+		}
+
+		control = MCA_WDT_ENABLE;
+		control |= priv->wdt_soft.no_way_out ? MCA_WDT_NOWAYOUT : 0;
+		control |= priv->wdt_soft.full_reset ? MCA_WDT_FULLRESET : 0;
+
+		ret = mca_update_bits(MCA_WDT_CONTROL,
+				      MCA_WDT_NOWAYOUT  | MCA_WDT_FULLRESET | MCA_WDT_ENABLE,
+				      control);
+		if (ret) {
+			printf("Couldn't configure watchdog (%d)\n", ret);
+			goto err;
+		}
+
+		mca_wdt_reset(dev);
 	}
 
-	control = MCA_WDT_ENABLE;
-	control |= priv->no_way_out ? MCA_WDT_NOWAYOUT : 0;
-	control |= priv->full_reset ? MCA_WDT_FULLRESET : 0;
+	for (i = 0 ; i < priv->wdt_gpio_num ; i++) {
+		u8 control;
 
-	ret = mca_update_bits(MCA_WDT_CONTROL,
-			      MCA_WDT_NOWAYOUT  | MCA_WDT_IRQNORESET |
-			      MCA_WDT_FULLRESET | MCA_WDT_ENABLE,
-			      control);
-	if (ret) {
-		printf("Could not configure watchdog (%d)\n", ret);
-		goto err;
+		ret = mca_write_reg(MCA_GPIO_WDT0_IO + 0x10*i,
+				    priv->wdt_gpio[i].mca_io_number);
+		if (ret) {
+			printf("Couldn't set gpio wdt %d io (%d)\n", i, ret);
+			goto err;
+		}
+
+		ret = mca_write_reg(MCA_GPIO_WDT0_TIMEOUT + 0x10*i,
+				    priv->wdt_gpio[i].timeout_sec);
+		if (ret) {
+			printf("Couldn't set gpio wdt %d timeout (%d)\n", i, ret);
+			goto err;
+		}
+
+		control = MCA_WDT_ENABLE;
+		control |= priv->wdt_gpio[i].no_way_out ? MCA_WDT_NOWAYOUT : 0;
+		control |= priv->wdt_gpio[i].full_reset ? MCA_WDT_FULLRESET : 0;
+		control |= priv->wdt_gpio[i].mode << MCA_GPIO_WDT_MODE_SHIFT;
+
+		ret = mca_update_bits(MCA_GPIO_WDT0_CONTROL + 0x10*i,
+				      MCA_WDT_NOWAYOUT  | MCA_WDT_FULLRESET | MCA_WDT_ENABLE |
+				      MCA_GPIO_WDT_MODE_MASK,
+				      control);
+		if (ret) {
+			printf("Couldn't configure gpio wdt %d (%d)\n", i, ret);
+			goto err;
+		}
 	}
-
-	mca_wdt_reset(dev);
 err:
 	return ret;
 }
@@ -84,9 +151,83 @@ err:
 static int mca_wdt_probe(struct udevice *dev)
 {
 	struct mca_wdt_priv *priv = dev_get_priv(dev);
+	ofnode node;
+	ofnode soft_rfsh, gpio_rfsh;
+	const char *mode, *name;
+	int ret;
+	u8 i = 0;
 
-	priv->full_reset = dev_read_bool(dev, "digi,full-reset");
-	priv->no_way_out = dev_read_bool(dev, "digi,no-way-out");
+	/* Parse standard soft-rfsh watchdog device tree entries */
+	soft_rfsh = dev_read_subnode(dev, "soft-rfsh");
+	/* Make sure soft_rfsh node was found. No Null check is available */
+	name = ofnode_get_name(soft_rfsh);
+	if (!strcmp(name, "soft-rfsh"))
+	{
+		priv->wdt_soft.enabled = true;
+		priv->wdt_soft.full_reset = ofnode_read_bool(soft_rfsh, "digi,full-reset");
+		priv->wdt_soft.no_way_out = ofnode_read_bool(soft_rfsh, "digi,no-way-out");
+	}
+
+	/* Parse gpio-rfsh watchdog device tree entries */
+	gpio_rfsh = dev_read_subnode(dev, "gpio-rfsh");
+	ofnode_for_each_subnode(node, gpio_rfsh) {
+		if (i == MCA_NUM_GPIO_WDG) {
+			printf("%s: Exceeds max num of mca_wdt_gpio nodes (%d)\n",
+				__func__, MCA_NUM_GPIO_WDG);
+			return -ERANGE;
+		}
+
+		priv->wdt_gpio[i].full_reset = ofnode_read_bool(node,
+								"digi,full-reset");
+		priv->wdt_gpio[i].no_way_out = ofnode_read_bool(node,
+								"digi,no-way-out");
+		ret = ofnode_read_u32(node, "digi,mca-io-number",
+				      &priv->wdt_gpio[i].mca_io_number);
+		if (ret) {
+			printf("%s: node %s has no mca-io-number\n",
+			       __func__, ofnode_get_name(node));
+			return -EINVAL;
+		}
+
+		ret = ofnode_read_u32(node, "digi,timeout-sec",
+				      &priv->wdt_gpio[i].timeout_sec);
+		if (ret) {
+			printf("%s: node %s has no digi,timeout-sec\n",
+			       __func__, ofnode_get_name(node));
+			return -EINVAL;
+		}
+		if (priv->wdt_gpio[i].timeout_sec < MCA_GPIO_WDG_MIN_TOUT_SEC ||
+		    priv->wdt_gpio[i].timeout_sec > MCA_GPIO_WDG_MAX_TOUT_SEC) {
+			printf("%s: node %s timeout-sec out of range. Set to %d\n",
+			       __func__, ofnode_get_name(node),
+			       MCA_GPIO_WDG_MAX_TOUT_SEC);
+			priv->wdt_gpio[i].timeout_sec = MCA_GPIO_WDG_MAX_TOUT_SEC;
+		}
+
+		mode = ofnode_read_string(node, "digi,mode");
+		if (!mode) {
+			printf("%s: node %s has no mode\n",
+			       __func__, ofnode_get_name(node));
+			return -EINVAL;
+		}
+
+		if (!strcmp(mode, "toggle")) {
+			priv->wdt_gpio[i].mode = MCA_GPIO_WD_TOGGLE;
+		} else if (!strcmp(mode, "level-high")) {
+			priv->wdt_gpio[i].mode = MCA_GPIO_WD_LEVEL_HIGH;
+		} else if (!strcmp(mode, "level-low")) {
+			priv->wdt_gpio[i].mode = MCA_GPIO_WD_LEVEL_LOW;
+		} else {
+			printf("%s: Invalid mode '%s'\n", __func__, mode);
+			return -EINVAL;
+		}
+
+		i++;
+	}
+	priv->wdt_gpio_num = i;
+
+	if (!priv->wdt_soft.enabled && !priv->wdt_gpio_num)
+		return -ENODEV;
 
 	return 0;
 }
