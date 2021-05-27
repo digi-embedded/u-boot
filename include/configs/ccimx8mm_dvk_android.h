56054ba0741567328be5e7055f6a7611d3d1ccd0
Javier Viguera <javier.viguera@digi.com>
2021-05-27 17:12:12 +0200

    ccimx8mm_dvk: move Android configuration to a new file
    At the moment this is cosmetic, no functional change as the configs in
    the new file are exactly the same to the ones removed in the "common"
    file.
    
    However, this change allows to override configuration done for DEY, as
    the inclusion of the android config file is done at the end of the
    device configuration, not on the 'common' file as before.
    
    Signed-off-by: Javier Viguera <javier.viguera@digi.com>

---
+++ include/configs/ccimx8mm_dvk_android.h
@@ -0,0 +1,21 @@
+#ifndef CCIMX8MM_DVK_ANDROID_H
+#define CCIMX8MM_DVK_ANDROID_H
+
+#define CONFIG_ANDROID_AB_SUPPORT
+#ifdef CONFIG_ANDROID_AB_SUPPORT
+#define CONFIG_SYSTEM_RAMDISK_SUPPORT
+#endif
+
+/* Fastboot BCB support uses 'do_raw_read' */
+#define CONFIG_CMD_READ
+
+/*
+ * Do not use encrypted lock
+ *
+ * This setting controls whether fastboot lock status is encrypted or not
+ * using the CAAM. At the moment using the CAAM for such task is failing
+ * (in CAAM-based 'decrypt_lock_store' function).
+ */
+#define NON_SECURE_FASTBOOT
+
+#endif /* CCIMX8MM_DVK_ANDROID_H */
