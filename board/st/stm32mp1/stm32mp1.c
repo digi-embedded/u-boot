d66d737eef8ae18068d7d1473b044b8eabd76521
Patrice Chotard <patrice.chotard@foss.st.com>
2021-08-02 16:04:51 +0200

    board: stm32mp1: Remove gpio_hog_probe_all() from board
    DM_GPIO_HOG flag has been replaced by GPIO_HOG flag since a while in
    commit 49b10cb49262 ("gpio: fixes for gpio-hog support").
    
    And furthermore, gpio_hog_probe_all() is already called in board_r.c.
    So gpio_hog_probe() can be removed from stm32mp1.c.
    
    Signed-off-by: Patrice Chotard <patrice.chotard@foss.st.com>
    Change-Id: I9beb7f8f4f69dbdb1764c9b9dc6d5275dcb6bbdb
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/u-boot/+/215686
    Reviewed-by: CIBUILD <MDG-smet-aci-builds@list.st.com>
    Reviewed-by: Patrick DELAUNAY <patrick.delaunay@foss.st.com>

--- board/st/stm32mp1/stm32mp1.c
+++ board/st/stm32mp1/stm32mp1.c
@@ -651,9 +651,6 @@ int board_init(void)
 	/* address of boot parameters */
 	gd->bd->bi_boot_params = STM32_DDR_BASE + 0x100;
 
-	if (CONFIG_IS_ENABLED(DM_GPIO_HOG))
-		gpio_hog_probe_all();
-
 	board_key_check();
 
 	if (board_is_ev1())
