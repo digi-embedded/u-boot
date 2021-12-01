5f2761e4233d00964d42cb0c03c56fa35b271820
Olivier Moysan <olivier.moysan@foss.st.com>
2021-12-01 11:28:49 +0100

    adc: stm32: add smart calibration support
    Add smart calibration support for STM32MP1.
    - STM32MP15x: both linear & offset calibration are supported
    - STM32MP13x: Only offset calibration is supported
    
    Linear calibration:
    Linear calibration is SoC dependent and does not change over
    time. As it is time consuming, do it only once.
    Restore calibration data from environment variable to save time.
    If no calibration data are found in u-boot environment variables
    run a new calibration.
    
    Offset calibration:
    This calibration is fast and may vary over time.
    Run offset single-ended and differential calibration on each boot.
    
    Signed-off-by: Olivier Moysan <olivier.moysan@foss.st.com>
    Change-Id: If8739d96e019d42341901c5da6a83925cc78333a
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/u-boot/+/231628
    Reviewed-by: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: Patrice CHOTARD <patrice.chotard@foss.st.com>
    Reviewed-by: Patrick DELAUNAY <patrick.delaunay@foss.st.com>

--- drivers/adc/stm32-adc.c
+++ drivers/adc/stm32-adc.c
@@ -9,6 +9,7 @@
 #include <common.h>
 #include <adc.h>
 #include <dm.h>
+#include <env.h>
 #include <asm/io.h>
 #include <dm/device_compat.h>
 #include <linux/bitops.h>
@@ -26,6 +27,8 @@
 #define STM32H7_ADC_SQR1		0x30
 #define STM32H7_ADC_DR			0x40
 #define STM32H7_ADC_DIFSEL		0xC0
+#define STM32H7_ADC_CALFACT		0xC4
+#define STM32H7_ADC_CALFACT2		0xC8
 
 /* STM32H7_ADC_ISR - bit fields */
 #define STM32MP1_VREGREADY		BIT(12)
@@ -37,11 +40,21 @@
 #define STM32H7_ADCALDIF		BIT(30)
 #define STM32H7_DEEPPWD			BIT(29)
 #define STM32H7_ADVREGEN		BIT(28)
+#define STM32H7_LINCALRDYW6		BIT(27)
+#define STM32H7_LINCALRDYW5		BIT(26)
+#define STM32H7_LINCALRDYW4		BIT(25)
+#define STM32H7_LINCALRDYW3		BIT(24)
+#define STM32H7_LINCALRDYW2		BIT(23)
+#define STM32H7_LINCALRDYW1		BIT(22)
 #define STM32H7_ADCALLIN		BIT(16)
 #define STM32H7_BOOST			BIT(8)
 #define STM32H7_ADSTART			BIT(2)
 #define STM32H7_ADDIS			BIT(1)
 #define STM32H7_ADEN			BIT(0)
+
+/* STM32H7_ADC_CALFACT2 - bit fields */
+#define STM32H7_LINCALFACT_SHIFT	0
+#define STM32H7_LINCALFACT_MASK		GENMASK(29, 0)
 
 /* STM32H7_ADC_CFGR bit fields */
 #define STM32H7_EXTEN			GENMASK(11, 10)
@@ -70,6 +83,9 @@
 
 #define STM32_ADC_CH_MAX		20	/* max number of channels */
 #define STM32_ADC_TIMEOUT_US		100000
+/* Number of linear calibration shadow registers / LINCALRDYW control bits */
+#define STM32H7_LINCALFACT_NUM		6
+#define STM32H7_LINCAL_NAME_LEN		32
 
 struct stm32_adc_cfg {
 	const struct stm32_adc_regspec	*regs;
@@ -85,6 +101,7 @@ struct stm32_adc {
 	void __iomem *regs;
 	int active_channel;
 	const struct stm32_adc_cfg *cfg;
+	u32 lincalfact[STM32H7_LINCALFACT_NUM];
 };
 
 struct stm32_adc_regs {
@@ -158,12 +175,28 @@ static int stm32_adc_stop(struct udevice *dev)
 	return 0;
 }
 
+static int stm32_adc_enable(struct udevice *dev)
+{
+	struct stm32_adc *adc = dev_get_priv(dev);
+	int ret;
+	u32 val;
+
+	setbits_le32(adc->regs + STM32H7_ADC_CR, STM32H7_ADEN);
+	ret = readl_poll_timeout(adc->regs + STM32H7_ADC_ISR, val,
+				 val & STM32H7_ADRDY, STM32_ADC_TIMEOUT_US);
+	if (ret < 0) {
+		stm32_adc_stop(dev);
+		dev_err(dev, "Failed to enable ADC: %d\n", ret);
+	}
+
+	return ret;
+}
+
 static int stm32_adc_start_channel(struct udevice *dev, int channel)
 {
 	struct adc_uclass_plat *uc_pdata = dev_get_uclass_plat(dev);
 	struct stm32_adc *adc = dev_get_priv(dev);
 	int ret;
-	u32 val;
 
 	ret = stm32_adc_exit_pwr_down(dev);
 	if (ret < 0)
@@ -172,15 +205,9 @@ static int stm32_adc_start_channel(struct udevice *dev, int channel)
 	/* Only use single ended channels */
 	clrbits_le32(adc->regs + adc->cfg->regs->difsel.reg, adc->cfg->regs->difsel.mask);
 
-	/* Enable ADC, Poll for ADRDY to be set (after adc startup time) */
-	setbits_le32(adc->regs + STM32H7_ADC_CR, STM32H7_ADEN);
-	ret = readl_poll_timeout(adc->regs + STM32H7_ADC_ISR, val,
-				 val & STM32H7_ADRDY, STM32_ADC_TIMEOUT_US);
-	if (ret < 0) {
-		stm32_adc_stop(dev);
-		dev_err(dev, "Failed to enable ADC: %d\n", ret);
+	ret = stm32_adc_enable(dev);
+	if (ret)
 		return ret;
-	}
 
 	/* Preselect channels */
 	if (adc->cfg->has_presel)
@@ -241,7 +268,7 @@ static int stm32_adc_channel_data(struct udevice *dev, int channel,
  */
 #define STM32H7_ADC_CALIB_TIMEOUT_US		100000
 
-static int stm32_adc_selfcalib(struct udevice *dev)
+static int stm32_adc_run_selfcalib(struct udevice *dev, int do_lincal)
 {
 	struct stm32_adc *adc = dev_get_priv(dev);
 	int ret;
@@ -250,7 +277,7 @@ static int stm32_adc_selfcalib(struct udevice *dev)
 	/*
 	 * Select calibration mode:
 	 * - Offset calibration for single ended inputs
-	 * - No linearity calibration. Done in next step.
+	 * - No linearity calibration.
 	 */
 	clrbits_le32(adc->regs + STM32H7_ADC_CR, STM32H7_ADCALDIF | STM32H7_ADCALLIN);
 
@@ -260,18 +287,18 @@ static int stm32_adc_selfcalib(struct udevice *dev)
 				       !(val & STM32H7_ADCAL), 100,
 				       STM32H7_ADC_CALIB_TIMEOUT_US);
 	if (ret) {
-		dev_err(dev, "calibration failed\n");
+		dev_err(dev, "calibration (offset single-ended) failed\n");
 		goto out;
 	}
 
 	/*
 	 * Select calibration mode, then start calibration:
 	 * - Offset calibration for differential input
-	 * - Linearity calibration (needs to be done only once for single/diff)
+	 * - Linearity calibration if not already done.
 	 *   will run simultaneously with offset calibration.
 	 */
 	mask = STM32H7_ADCALDIF;
-	if (adc->cfg->has_linearcal)
+	if (adc->cfg->has_linearcal && do_lincal)
 		mask |= STM32H7_ADCALLIN;
 	setbits_le32(adc->regs + STM32H7_ADC_CR, mask);
 
@@ -281,11 +308,178 @@ static int stm32_adc_selfcalib(struct udevice *dev)
 				       !(val & STM32H7_ADCAL), 100,
 				       STM32H7_ADC_CALIB_TIMEOUT_US);
 	if (ret)
-		dev_err(dev, "calibration failed\n");
+		dev_err(dev, "calibration (offset diff%s) failed\n",
+			(mask & STM32H7_ADCALLIN) ? "+linear" : "");
 
 out:
 	clrbits_le32(adc->regs + STM32H7_ADC_CR, STM32H7_ADCALDIF | STM32H7_ADCALLIN);
 
+	return ret;
+}
+
+/* Retrieve calibration data from env variables */
+static bool stm32_adc_getenv_selfcalib(struct udevice *dev)
+{
+	struct stm32_adc *adc = dev_get_priv(dev);
+	char env_name[STM32H7_LINCAL_NAME_LEN];
+	char *env;
+	int i;
+
+	memset(&adc->lincalfact, 0, STM32H7_LINCALFACT_NUM * sizeof(u32));
+	for (i = STM32H7_LINCALFACT_NUM - 1; i >= 0; i--) {
+		/*
+		 * Save ADC linear calibration factors in U-boot environment variables
+		 * Variables are instantiated according to the adc address through
+		 * adcx_ prefix.
+		 */
+		snprintf(env_name, sizeof(env_name), "adc%x_lincalfact%d", (u32)adc->regs, i + 1);
+		env = env_get(env_name);
+		if (!env)
+			return false;
+		adc->lincalfact[i] = env_get_hex(env_name, 0);
+	}
+	return true;
+}
+
+/* Save calibration data to env variables */
+static void stm32_adc_save_selfcalib(struct udevice *dev)
+{
+	struct stm32_adc *adc = dev_get_priv(dev);
+	char env_name[STM32H7_LINCAL_NAME_LEN];
+	int i;
+
+	for (i = STM32H7_LINCALFACT_NUM - 1; i >= 0; i--) {
+		snprintf(env_name, sizeof(env_name), "adc%x_lincalfact%d", (u32)adc->regs, i + 1);
+		if (env_set_hex(env_name, adc->lincalfact[i]))
+			dev_warn(dev, "Failed to save %s\n", env_name);
+	}
+}
+
+/* Read calibration data from ADC */
+static int stm32_adc_read_selfcalib(struct udevice *dev)
+{
+	struct stm32_adc *adc = dev_get_priv(dev);
+	u32 lincalrdyw_mask, val;
+	int i, ret;
+
+	/* Read linearity calibration */
+	lincalrdyw_mask = STM32H7_LINCALRDYW6;
+	for (i = STM32H7_LINCALFACT_NUM - 1; i >= 0; i--) {
+		/* Clear STM32H7_LINCALRDYW[6..1]: transfer calib to CALFACT2 */
+		clrbits_le32(adc->regs + STM32H7_ADC_CR, lincalrdyw_mask);
+
+		/* Poll: wait calib data to be ready in CALFACT2 register */
+		ret = readl_poll_sleep_timeout(adc->regs + STM32H7_ADC_CR, val,
+					       !(val & lincalrdyw_mask), 100,
+					       STM32_ADC_TIMEOUT_US);
+		if (ret) {
+			dev_err(dev, "Failed to read calfact\n");
+			return ret;
+		}
+
+		val = readl(adc->regs + STM32H7_ADC_CALFACT2);
+		adc->lincalfact[i] = (val & STM32H7_LINCALFACT_MASK);
+		adc->lincalfact[i] >>= STM32H7_LINCALFACT_SHIFT;
+
+		lincalrdyw_mask >>= 1;
+	}
+
+	return 0;
+}
+
+/* Write calibration data to ADC */
+static int stm32_adc_write_selfcalib(struct udevice *dev)
+{
+	struct stm32_adc *adc = dev_get_priv(dev);
+	u32 lincalrdyw_mask, val;
+	int i, ret;
+
+	lincalrdyw_mask = STM32H7_LINCALRDYW6;
+	for (i = STM32H7_LINCALFACT_NUM - 1; i >= 0; i--) {
+		/*
+		 * Write saved calibration data to shadow registers:
+		 * Write CALFACT2, and set LINCALRDYW[6..1] bit to trigger
+		 * data write. Then poll to wait for complete transfer.
+		 */
+		val = adc->lincalfact[i] << STM32H7_LINCALFACT_SHIFT;
+		writel(val, adc->regs + STM32H7_ADC_CALFACT2);
+		setbits_le32(adc->regs + STM32H7_ADC_CR, lincalrdyw_mask);
+		ret = readl_poll_sleep_timeout(adc->regs + STM32H7_ADC_CR, val,
+					       val & lincalrdyw_mask,
+					       100, STM32_ADC_TIMEOUT_US);
+		if (ret) {
+			dev_err(dev, "Failed to write calfact\n");
+			return ret;
+		}
+
+		lincalrdyw_mask >>= 1;
+	}
+
+	return 0;
+}
+
+static int stm32_adc_selfcalib(struct udevice *dev)
+{
+	struct stm32_adc *adc = dev_get_priv(dev);
+	int ret;
+	bool lincal_done = false;
+
+	/* Try to restore linear calibration */
+	if (adc->cfg->has_linearcal)
+		lincal_done = stm32_adc_getenv_selfcalib(dev);
+
+	/*
+	 * Run offset calibration unconditionally.
+	 * Run linear calibration if not already available.
+	 */
+	ret = stm32_adc_run_selfcalib(dev, !lincal_done);
+	if (ret)
+		return ret;
+
+	ret = stm32_adc_enable(dev);
+	if (ret)
+		return ret;
+
+	if (adc->cfg->has_linearcal) {
+		if (!lincal_done) {
+			ret = stm32_adc_read_selfcalib(dev);
+			if (ret)
+				goto disable;
+
+			stm32_adc_save_selfcalib(dev);
+		}
+
+		/*
+		 * Always write linear calibration data to ADC.
+		 * This allows to ensure that LINCALRDYWx bits are set when entering kernel
+		 *
+		 * - First boot:
+		 *   U-boot performs ADC linear calibration (& offset calibration)
+		 *   U-boot reads & saves linear calibration result in environment variable
+		 *   (Here LINCALRDYWx have been cleared due to the read procedure)
+		 *   U-boot writes back ADC linear calibration to set LINCALRDYWx bits,
+		 *   making the linear calibration available for the kernel.
+		 *
+		 * - Subsequent boot (environment set earlier):
+		 *   U-boot performs ADC offset calibration only
+		 *   U-boot reads ADC linear calibration from environment variable
+		 *   and writes back ADC linear calibration.
+		 *
+		 * - All boot: kernel steps
+		 *   * Case1: ADC calibrated by U-boot (LINCALRDYWx bits set)
+		 *     Read back the linear calibration from ADC registers and save it.
+		 *   * Case2: ADC not calibrated by U-boot
+		 *     Run a linear calibration and save it.
+		 */
+		ret = stm32_adc_write_selfcalib(dev);
+		if (ret)
+			goto disable;
+	}
+
+	return ret;
+
+disable:
+	stm32_adc_stop(dev);
 	return ret;
 }
 
