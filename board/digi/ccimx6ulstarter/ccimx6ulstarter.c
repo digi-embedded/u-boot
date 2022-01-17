1f8116a53f233eb531ca207b2a783dfc3c2ff391
Hector Palacios <hector.palacios@digi.com>
2022-01-17 08:55:16 +0100

    mx6 Kconfig: enable module fuse check options
    Enable the configurations CONFIG_MODULE_FUSE and
    CONFIG_OF_SYSTEM_SETUP following NXP commit
    bdf0029e0facf763dd1f9555231caf7da48860fb.
    
    Update renamed fuse constants on the board code.
    
    Signed-off-by: Hector Palacios <hector.palacios@digi.com>

--- board/digi/ccimx6ulstarter/ccimx6ulstarter.c
+++ board/digi/ccimx6ulstarter/ccimx6ulstarter.c
@@ -296,14 +296,14 @@ static int setup_fec(int fec_id)
 	}
 
 	if (0 == fec_id) {
-		if (check_module_fused(MX6_MODULE_ENET1))
+		if (check_module_fused(MODULE_ENET1))
 			return -1;
 
 		/* Use 50M anatop loopback REF_CLK1 for ENET1, clear gpr1[13], set gpr1[17]*/
 		clrsetbits_le32(&iomuxc_gpr_regs->gpr[1], IOMUX_GPR1_FEC1_MASK,
 				IOMUX_GPR1_FEC1_CLOCK_MUX1_SEL_MASK);
 	} else {
-		if (check_module_fused(MX6_MODULE_ENET2))
+		if (check_module_fused(MODULE_ENET2))
 			return -1;
 
 		/* Use 50M anatop loopback REF_CLK2 for ENET2, clear gpr1[14], set gpr1[18]*/
