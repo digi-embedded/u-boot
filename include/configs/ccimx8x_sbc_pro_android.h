4e7ab450a4cd49ef67471ef64f63fa9ef23962d5
Hector Palacios <hector.palacios@digi.com>
2022-02-01 13:37:45 +0100

    ccimx8x/8m android: update defconfigs for Android support
    - Move CONFIG_ANDROID_SUPPORT and other related configurations from header
      into defconfig files to avoid redefinition build errors.
    - Sync defconfig after a savedefconfig operation.
    
    Signed-off-by: Hector Palacios <hector.palacios@digi.com>

--- include/configs/ccimx8x_sbc_pro_android.h
+++ include/configs/ccimx8x_sbc_pro_android.h
@@ -3,11 +3,6 @@
 
 #define CONFIG_USB_GADGET_VBUS_DRAW	2
 
-#define CONFIG_ANDROID_AB_SUPPORT
-#ifdef CONFIG_ANDROID_AB_SUPPORT
-#define CONFIG_SYSTEM_RAMDISK_SUPPORT
-#endif
-
 /* Disable some configs not needed for Android */
 #undef CONFIG_BOOTCOMMAND
 #undef CONFIG_FSL_CAAM_KB
@@ -21,7 +16,6 @@
 #ifdef CONFIG_SPL_BUILD
 #undef CONFIG_BLK
 #define CONFIG_FSL_CAAM_KB
-#define CONFIG_SPL_CRYPTO_SUPPORT
 #endif
 #endif
 
