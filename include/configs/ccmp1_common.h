5fb3d207f56a3654c7eb5b50ac3f30ec1622b334
Hector Palacios <hector.palacios@digi.com>
2022-02-03 16:50:38 +0100

    ccmp1: add MCA support
    The CCMP1 assembles a KL03 MCA on bus I2C4.
    
    Signed-off-by: Hector Palacios <hector.palacios@digi.com>

--- include/configs/ccmp1_common.h
+++ include/configs/ccmp1_common.h
@@ -9,4 +9,10 @@
 
 #include "digi_common.h"
 
+/* MCA */
+#define CONFIG_MCA_I2C_BUS		3
+#define CONFIG_MCA_I2C_ADDR		0x63
+#define CONFIG_MCA_OFFSET_LEN		2
+#define BOARD_MCA_DEVICE_ID		0x61
+
 #endif /* __CCMP1_COMMON_H */
