dcc3e020b87c6e3c1c2287d2bca8e3c8d9e7fa2c
Patrick Delaunay <patrick.delaunay@foss.st.com>
2022-03-09 17:47:51 +0100

    stm32mp: bsec: add support of stm32mp25
    Add support of BSEC for STM32MP25x family to access OTP.
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Change-Id: I81fedd3d2ff9635b3738dd1ef349c936bc9b433d
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/u-boot/+/244280
    Reviewed-by: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: Patrice CHOTARD <patrice.chotard@foss.st.com>

--- arch/arm/mach-stm32mp/bsec.c
+++ arch/arm/mach-stm32mp/bsec.c
@@ -783,9 +783,16 @@ static const struct stm32mp_bsec_drvdata stm32mp15_data = {
 	.size = 96,
 	.ta = false,
 };
+
+static const struct stm32mp_bsec_drvdata stm32mp25_data = {
+	.size = 367, /* 384 but no access to HWKEY and STM32PRVKEY */
+	.ta = true,
+};
+
 static const struct udevice_id stm32mp_bsec_ids[] = {
 	{ .compatible = "st,stm32mp13-bsec", .data = (ulong)&stm32mp13_data},
 	{ .compatible = "st,stm32mp15-bsec", .data = (ulong)&stm32mp15_data},
+	{ .compatible = "st,stm32mp25-bsec", .data = (ulong)&stm32mp25_data},
 	{}
 };
 
