51cbb48831e5159de7ee8c99fe42d6eb45985e4a
Hector Palacios <hector.palacios@digi.com>
2022-05-16 11:11:44 +0200

    ccmp15: move common defines to ccmp1_common.h
    Signed-off-by: Hector Palacios <hector.palacios@digi.com>

--- include/configs/ccmp15_dvk.h
+++ include/configs/ccmp15_dvk.h
@@ -10,85 +10,6 @@
 
 #include <configs/ccmp1_common.h>
 
-/*MMC SD*/
-#define CONFIG_SYS_MMC_MAX_DEVICE	3
-
-/* NAND support */
-#define CONFIG_SYS_NAND_ONFI_DETECTION
-#define CONFIG_SYS_MAX_NAND_DEVICE	1
-
-/* Ethernet need */
-#ifdef CONFIG_DWC_ETH_QOS
-#define CONFIG_SERVERIP                 192.168.1.1
-#define CONFIG_BOOTP_SERVERIP
-#define CONFIG_SYS_AUTOLOAD		"no"
-#endif
-
-/*****************************************************************************/
-#ifdef CONFIG_DISTRO_DEFAULTS
-/*****************************************************************************/
-
-#if !defined(CONFIG_SPL_BUILD)
-
-#ifdef CONFIG_CMD_MMC
-#define BOOT_TARGET_MMC0(func)	func(MMC, mmc, 0)
-#define BOOT_TARGET_MMC1(func)	func(MMC, mmc, 1)
-#define BOOT_TARGET_MMC2(func)	func(MMC, mmc, 2)
-#else
-#define BOOT_TARGET_MMC0(func)
-#define BOOT_TARGET_MMC1(func)
-#define BOOT_TARGET_MMC2(func)
-#endif
-
-#ifdef CONFIG_NET
-#define BOOT_TARGET_PXE(func)	func(PXE, pxe, na)
-#else
-#define BOOT_TARGET_PXE(func)
-#endif
-
-#ifdef CONFIG_CMD_UBIFS
-#define BOOT_TARGET_UBIFS(func)	func(UBIFS, ubifs, 0)
-#else
-#define BOOT_TARGET_UBIFS(func)
-#endif
-
-#ifdef CONFIG_USB
-#define BOOT_TARGET_USB(func)	func(USB, usb, 0)
-#else
-#define BOOT_TARGET_USB(func)
-#endif
-
-#define BOOT_TARGET_DEVICES(func)	\
-	BOOT_TARGET_MMC1(func)		\
-	BOOT_TARGET_UBIFS(func)		\
-	BOOT_TARGET_MMC0(func)		\
-	BOOT_TARGET_MMC2(func)		\
-	BOOT_TARGET_USB(func)		\
-	BOOT_TARGET_PXE(func)
-
-/*
- * bootcmd for stm32mp1:
- * for serial/usb: execute the stm32prog command
- * for mmc boot (eMMC, SD card), boot only on the same device
- * for nand or spi-nand boot, boot with on ubifs partition on UBI partition
- * for nor boot, use SD card = mmc0
- */
-#define STM32MP_BOOTCMD "bootcmd_stm32mp=" \
-	"echo \"Boot over ${boot_device}${boot_instance}!\";" \
-	"if test ${boot_device} = serial || test ${boot_device} = usb;" \
-	"then stm32prog ${boot_device} ${boot_instance}; " \
-	"else " \
-		"run env_check;" \
-		"if test ${boot_device} = mmc;" \
-		"then env set boot_targets \"mmc${boot_instance}\"; fi;" \
-		"if test ${boot_device} = nand ||" \
-		  " test ${boot_device} = spi-nand ;" \
-		"then env set boot_targets ubifs0; fi;" \
-		"if test ${boot_device} = nor;" \
-		"then env set boot_targets mmc0; fi;" \
-		"run distro_bootcmd;" \
-	"fi;\0"
-
 #ifdef CONFIG_FASTBOOT_CMD_OEM_FORMAT
 /* eMMC default partitions for fastboot command: oem format */
 #define PARTS_DEFAULT \
@@ -101,8 +22,6 @@
 #else
 #define PARTS_DEFAULT
 #endif
-
-#include <config_distro_bootcmd.h>
 
 /*
  * memory layout for 32M uncompressed/compressed kernel,
@@ -124,7 +43,5 @@
 	BOOTENV \
 	"boot_net_usb_start=true\0"
 
-#endif /* ifndef CONFIG_SPL_BUILD */
-#endif /* ifdef CONFIG_DISTRO_DEFAULTS*/
 
 #endif /* __CONFIG_H */
