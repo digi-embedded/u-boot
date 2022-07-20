873d895ce35887720de7d2b194b0c7c04668af0d
Christophe Kerello <christophe.kerello@foss.st.com>
2022-07-20 17:30:18 +0200

    board: st: stm32mp1: use of correct compatible string to add partitions
    Current compatible string used to update SPI NAND and SPI NOR devices
    can lead to a wrong partitions update (for example, SPI NAND partitions
    added to SPI NOR node in the device tree). To avoid this wrong behavior,
    use jedec,spi-nor compatible string for SPI NOR devices and spi-nand
    compatible string for SPI NAND devices.
    
    Change-Id: Iae28ab1a0be932b26f9cf8b17d870508efa88b79
    Signed-off-by: Christophe Kerello <christophe.kerello@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/u-boot/+/260967
    Reviewed-by: Christophe KERELLO <christophe.kerello@st.com>
    Reviewed-by: Patrice CHOTARD <patrice.chotard@foss.st.com>
    Reviewed-by: Patrick DELAUNAY <patrick.delaunay@foss.st.com>
    Tested-by: Christophe KERELLO <christophe.kerello@st.com>

--- board/st/stm32mp1/stm32mp1.c
+++ board/st/stm32mp1/stm32mp1.c
@@ -1434,8 +1434,8 @@ void fdt_update_panel_dsi(void *new_blob)
 int ft_board_setup(void *blob, struct bd_info *bd)
 {
 	static const struct node_info nodes[] = {
-		{ "st,stm32f469-qspi",		MTD_DEV_TYPE_NOR,  },
-		{ "st,stm32f469-qspi",		MTD_DEV_TYPE_SPINAND},
+		{ "jedec,spi-nor",		MTD_DEV_TYPE_NOR,  },
+		{ "spi-nand",			MTD_DEV_TYPE_SPINAND},
 		{ "st,stm32mp15-fmc2",		MTD_DEV_TYPE_NAND, },
 		{ "st,stm32mp1-fmc2-nfc",	MTD_DEV_TYPE_NAND, },
 	};
