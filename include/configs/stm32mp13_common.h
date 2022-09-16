093b8d1db5387a5e0d941ecf56d01f5c198d5e7b
Patrick Delaunay <patrick.delaunay@foss.st.com>
2022-09-16 14:52:32 +0200

    configs: stm32mp13: activate UBI support
    Activate UBI support for NOR and NAND devices.
    
    Change-Id: Ia856c28353497a6a283dd1b514e57a49054d56dc
    Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/u-boot/+/270275
    Reviewed-by: CITOOLS <MDG-smet-aci-reviews@list.st.com>

--- include/configs/stm32mp13_common.h
+++ include/configs/stm32mp13_common.h
@@ -39,19 +39,30 @@
 #define BOOT_TARGET_MMC1(func)
 #endif
 
+#ifdef CONFIG_CMD_UBIFS
+#define BOOT_TARGET_UBIFS(func)	func(UBIFS, ubifs, 0, UBI, boot)
+#else
+#define BOOT_TARGET_UBIFS(func)
+#endif
+
 #define BOOT_TARGET_DEVICES(func)	\
 	BOOT_TARGET_MMC1(func)		\
+	BOOT_TARGET_UBIFS(func)		\
 	BOOT_TARGET_MMC0(func)
 
 /*
  * default bootcmd for stm32mp13:
  * for mmc boot (eMMC, SD card), distro boot on the same mmc device
+ * for nand or spi-nand boot, distro boot with ubifs on UBI partition
  */
 #define STM32MP_BOOTCMD "bootcmd_stm32mp=" \
 	"echo \"Boot over ${boot_device}${boot_instance}!\";" \
 		"run env_check;" \
 		"if test ${boot_device} = mmc;" \
 		"then env set boot_targets \"mmc${boot_instance}\"; fi;" \
+		"if test ${boot_device} = nand ||" \
+		  " test ${boot_device} = spi-nand ;" \
+		"then env set boot_targets ubifs0; fi;" \
 		"run distro_bootcmd;" \
 	"fi;\0"
 
