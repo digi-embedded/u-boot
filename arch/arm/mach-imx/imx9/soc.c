687b3151cf55b4df6016ca5c15ef82950e94ecd1
Javier Viguera <javier.viguera@digi.com>
2022-09-27 14:23:44 +0200

    mach-imx: imx9: fix build without CONFIG_IMX_OPTEE
    Port equivalent change for the imx8m in commit
    d4fcccd3456b32e53a31f6b36c9cd59e0f20abe4.
    
    Fix: undefined reference to `ft_add_optee_node'
    
    Signed-off-by: Javier Viguera <javier.viguera@digi.com>

--- arch/arm/mach-imx/imx9/soc.c
+++ arch/arm/mach-imx/imx9/soc.c
@@ -17,6 +17,9 @@
 #include <asm/arch/sys_proto.h>
 #include <asm/arch/trdc.h>
 #include <asm/mach-imx/boot_mode.h>
+#ifdef CONFIG_IMX_OPTEE
+#include <asm/mach-imx/optee.h>
+#endif
 #include <asm/mach-imx/syscounter.h>
 #include <asm/armv8/mmu.h>
 #include <dm/uclass.h>
@@ -335,7 +338,11 @@ int arch_misc_init(void)
 
 int ft_system_setup(void *blob, struct bd_info *bd)
 {
+#ifdef CONFIG_IMX_OPTEE
 	return ft_add_optee_node(blob, bd);
+#else
+	return 0;
+#endif
 }
 
 #if defined(CONFIG_SERIAL_TAG) || defined(CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG)
