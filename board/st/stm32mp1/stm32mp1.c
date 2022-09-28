b5e90d788f075b5e1bd17a89dcade926a44b906e
Patrick Delaunay <patrick.delaunay@foss.st.com>
2022-09-28 14:38:17 +0200

    board: stm32mp1: use fdt_copy_fixed_partitions
    Copy the fixed partition nodes from U-Boot device tree to Linux kernel
    device tree to dynamically configure the MTD partitions.
    
    fdt_copy_fixed_partitions is only based on device tree
    and replace the function fdt_fixup_mtdparts based on mtdparts variable;
    the variable mtdid and mtdparts are not more required.
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Change-Id: I7bf81c0580c1c801def7453d5d05c323a1b8d620
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/u-boot/+/288900
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
    Reviewed-by: Patrice CHOTARD <patrice.chotard@foss.st.com>
    Domain-Review: Patrice CHOTARD <patrice.chotard@foss.st.com>

--- board/st/stm32mp1/stm32mp1.c
+++ board/st/stm32mp1/stm32mp1.c
@@ -25,7 +25,6 @@
 #include <log.h>
 #include <malloc.h>
 #include <misc.h>
-#include <mtd_node.h>
 #include <net.h>
 #include <netdev.h>
 #include <phy.h>
@@ -1185,20 +1184,7 @@ void fdt_update_panel_dsi(void *new_blob)
 
 int ft_board_setup(void *blob, struct bd_info *bd)
 {
-	static const struct node_info nodes[] = {
-		{ "jedec,spi-nor",		MTD_DEV_TYPE_NOR,  },
-		{ "spi-nand",			MTD_DEV_TYPE_SPINAND},
-		{ "st,stm32mp15-fmc2",		MTD_DEV_TYPE_NAND, },
-		{ "st,stm32mp1-fmc2-nfc",	MTD_DEV_TYPE_NAND, },
-	};
-	char *boot_device;
-
-	/* Check the boot-source and don't update MTD for serial or usb boot */
-	boot_device = env_get("boot_device");
-	if (!boot_device ||
-	    (strcmp(boot_device, "serial") && strcmp(boot_device, "usb")))
-		if (IS_ENABLED(CONFIG_FDT_FIXUP_PARTITIONS))
-			fdt_fixup_mtdparts(blob, nodes, ARRAY_SIZE(nodes));
+	fdt_copy_fixed_partitions(blob);
 
 	if (CONFIG_IS_ENABLED(FDT_SIMPLEFB))
 		fdt_simplefb_enable_and_mem_rsv(blob);
