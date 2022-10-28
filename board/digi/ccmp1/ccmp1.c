427bafd20e1b11bc9568c0a71b99e80396786fd7
Gonzalo Ruiz <Gonzalo.Ruiz@digi.com>
2022-10-28 09:58:24 +0200

    ccmp1: fix serial# definition to include family ID
    Serial number does not include the 'Generator ID' from the HWID.
    Instead, it includes 2 digits of the family ID:
    - '01' for CC8X
    - '02' for CC8M Nano
    - '03' for CC8M Mini
    - '04' for CCMP1
    
    Set the correct family ID field.
    
    Signed-off-by: Gonzalo Ruiz <Gonzalo.Ruiz@digi.com>

--- board/digi/ccmp1/ccmp1.c
+++ board/digi/ccmp1/ccmp1.c
@@ -141,8 +141,7 @@ void som_default_environment(void)
 		verify_mac_address("btaddr", DEFAULT_MAC_BTADDR);
 
 	/* Get serial number from fuses */
-	hwid_get_serial_number(my_hwid.year, my_hwid.week,
-			       my_hwid.genid, my_hwid.sn);
+	hwid_get_serial_number(my_hwid.year, my_hwid.week, my_hwid.sn);
 }
 
 void board_update_hwid(bool is_fuse)
--- board/digi/common/hwid.h
+++ board/digi/common/hwid.h
@@ -46,6 +46,6 @@ void fdt_fixup_hwid(void *fdt, const struct digi_hwid *hwid);
 u32 hwid_get_ramsize(const struct digi_hwid *hwid);
 void print_hwid_hex(struct digi_hwid *hwid);
 void hwid_get_macs(uint32_t pool, uint32_t base);
-void hwid_get_serial_number(uint32_t year, uint32_t week, uint32_t genid, uint32_t serial);
+void hwid_get_serial_number(uint32_t year, uint32_t week, uint32_t serial);
 
 #endif	/* __HWID_H_ */
