e9e6cd4852c1e125f21c5d5f09cb454162ffd0c3
Mike Engel <Mike.Engel@digi.com>
2023-07-21 09:37:45 +0200

    board: digi: create common env encryption function
    This commit creates a weak env encryption function.
    That can be overwritten by each board implementation
    depeding on encryption support for each platform.
    
    Signed-off-by: Mike Engel <Mike.Engel@digi.com>

--- env/common.c
+++ env/common.c
@@ -122,70 +122,10 @@ int env_set_default_vars(int nvars, char * const vars[], int flags)
 				flags, 0, nvars, vars);
 }
 
-#ifdef CONFIG_ENV_AES_CAAM_KEY
-#include <fuse.h>
-#include <fsl_caam.h>
-#include <u-boot/md5.h>
-#include <asm/mach-imx/hab.h>
-#include "../board/digi/common/trustfence.h"
-
-static int env_aes_cbc_crypt(env_t *env, const int enc)
-{
-	unsigned char *data = env->data;
-	unsigned char *buffer;
-	int ret = 0;
-	unsigned char key_modifier[KEY_MODIFER_SIZE] = {0};
-
-	if (!imx_hab_is_enabled())
-		return 0;
-
-	ret = get_trustfence_key_modifier(key_modifier);
-	if (ret)
-		return ret;
-
-	caam_open();
-	buffer = malloc(ENV_SIZE);
-	if (!buffer) {
-		debug("Not enough memory for en/de-cryption buffer");
-		return -ENOMEM;
-	}
-
-	if (enc)
-		ret = caam_gen_blob((ulong)data, (ulong)buffer, key_modifier, ENV_SIZE - BLOB_OVERHEAD);
-	else
-		ret = caam_decap_blob((ulong)buffer, (ulong)data, key_modifier, ENV_SIZE - BLOB_OVERHEAD);
-
-	if (ret)
-		goto err;
-
-	memcpy(data, buffer, ENV_SIZE);
-
-err:
-	free(buffer);
-	return ret;
-}
-#elif CONFIG_ENV_AES_CCMP1
-static int env_aes_cbc_crypt(env_t *env, const int enc)
-{
-	unsigned char *data = env->data;
-	int ret = 0;
-	/* get NAND geometrics */
-	struct mtd_info *mtd = get_nand_dev_by_index(0);
-
-	if (enc) {
-		crypt_cipher_data(TA_AES_MODE_ENCODE, data, mtd->erasesize);
-	} else {
-		crypt_cipher_data(TA_AES_MODE_DECODE, data, mtd->erasesize);
-	}
-
-	return ret;
-}
-#else
-static inline int env_aes_cbc_crypt(env_t *env, const int enc)
+__weak int env_aes_cbc_crypt(env_t *env, const int enc)
 {
 	return 0;
 }
-#endif /* CONFIG_ENV_AES_CAAM_KEY */
 
 /*
  * Check if CRC is valid and (if yes) import the environment.
