cb37652960de70e95976dd3b2b35120f9e095b23
nsaldarriaga <nsaldarriaga@alvic.net>
2023-07-25 17:04:54 +0200

    On branch v2022.04/maint  Your branch is up to date with 'origin/v2022.04/maint'.
     Changes to be committed:
    	new file:   board/digi/ccimx6ulalvic/Kconfig
    	new file:   board/digi/ccimx6ulalvic/MAINTAINERS
    	new file:   board/digi/ccimx6ulalvic/Makefile
    	new file:   board/digi/ccimx6ulalvic/ccimx6ulstarter.c
    	modified:   board/digi/ccimx6ulstarter/MAINTAINERS
    	modified:   board/digi/ccimx6ulstarter/Makefile
    	new file:   configs/ccimx6ulalvic_defconfig
    	new file:   include/configs/ccimx6ulalvic.h

---
+++ board/digi/ccimx6ulalvic/ccimx6ulstarter.c
@@ -0,0 +1,408 @@
+/*
+ * Copyright (C) 2016-2018 Digi International, Inc
+ * Copyright (C) 2015 Freescale Semiconductor, Inc.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <init.h>
+#include <net.h>
+#include <asm/arch/clock.h>
+#include <asm/arch/iomux.h>
+#include <asm/arch/imx-regs.h>
+#include <asm/arch/crm_regs.h>
+#include <asm/arch/mx6ul_pins.h>
+#include <asm/arch/mx6-pins.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/global_data.h>
+#include <asm/gpio.h>
+#include <asm/mach-imx/iomux-v3.h>
+#include <asm/mach-imx/boot_mode.h>
+#include <asm/mach-imx/mxc_i2c.h>
+#include <asm/io.h>
+#include <common.h>
+#include <env.h>
+#include <fsl_esdhc_imx.h>
+#include <i2c.h>
+#include <linux/delay.h>
+#include <linux/sizes.h>
+#include <linux/fb.h>
+#include <miiphy.h>
+#include <mmc.h>
+#include <mxsfb.h>
+#include <netdev.h>
+#include <usb.h>
+#include <usb/ehci-ci.h>
+#include <command.h>
+#include "../ccimx6ul/ccimx6ul.h"
+#include "../common/carrier_board.h"
+#include "../common/helper.h"
+#include "../common/mca_registers.h"
+#include "../common/mca.h"
+#include "../common/trustfence.h"
+
+#ifdef CONFIG_POWER
+#include <power/pmic.h>
+#include <power/pfuze3000_pmic.h>
+#include "../../freescale/common/pfuze.h"
+#endif
+
+#ifdef CONFIG_FSL_FASTBOOT
+#include <fastboot.h>
+#ifdef CONFIG_ANDROID_RECOVERY
+#include <recovery.h>
+#endif
+#endif /*CONFIG_FSL_FASTBOOT*/
+
+DECLARE_GLOBAL_DATA_PTR;
+
+unsigned int board_version = CARRIERBOARD_VERSION_UNDEFINED;
+unsigned int board_id = CARRIERBOARD_ID_UNDEFINED;
+
+#define UART_PAD_CTRL  (PAD_CTL_PKE | PAD_CTL_PUE |		\
+	PAD_CTL_PUS_100K_UP | PAD_CTL_SPEED_MED |		\
+	PAD_CTL_DSE_40ohm   | PAD_CTL_SRE_FAST  | PAD_CTL_HYS)
+
+#define I2C_PAD_CTRL    (PAD_CTL_PKE | PAD_CTL_PUE |            \
+	PAD_CTL_PUS_100K_UP | PAD_CTL_SPEED_MED |               \
+	PAD_CTL_DSE_40ohm | PAD_CTL_HYS |			\
+	PAD_CTL_ODE)
+
+#define USDHC_PAD_CTRL (PAD_CTL_PKE | PAD_CTL_PUE |		\
+	PAD_CTL_PUS_22K_UP  | PAD_CTL_SPEED_LOW |		\
+	PAD_CTL_DSE_80ohm   | PAD_CTL_SRE_FAST  | PAD_CTL_HYS)
+
+#define ENET_PAD_CTRL  (PAD_CTL_PUS_100K_UP | PAD_CTL_PUE |     \
+	PAD_CTL_SPEED_HIGH   |                                   \
+	PAD_CTL_DSE_48ohm   | PAD_CTL_SRE_FAST)
+
+#define MDIO_PAD_CTRL  (PAD_CTL_PUS_100K_UP | PAD_CTL_PUE |     \
+	PAD_CTL_DSE_48ohm   | PAD_CTL_SRE_FAST | PAD_CTL_ODE)
+
+#define ENET_CLK_PAD_CTRL  (PAD_CTL_DSE_40ohm   | PAD_CTL_SRE_FAST)
+
+#define ENET_RX_PAD_CTRL  (PAD_CTL_PKE | PAD_CTL_PUE |          \
+	PAD_CTL_SPEED_HIGH   | PAD_CTL_SRE_FAST)
+
+#define OTG_ID_PAD_CTRL (PAD_CTL_PKE | PAD_CTL_PUE |		\
+	PAD_CTL_PUS_47K_UP  | PAD_CTL_SPEED_LOW |		\
+	PAD_CTL_DSE_80ohm   | PAD_CTL_SRE_FAST  | PAD_CTL_HYS)
+
+#define GPI_PAD_CTRL  (PAD_CTL_PKE | PAD_CTL_PUE |            \
+	PAD_CTL_PUS_100K_DOWN | PAD_CTL_SPEED_MED |               \
+	PAD_CTL_DSE_40ohm   | PAD_CTL_SRE_FAST)
+
+static iomux_v3_cfg_t const uart5_pads[] = {
+	MX6_PAD_UART5_TX_DATA__UART5_DCE_TX | MUX_PAD_CTRL(UART_PAD_CTRL),
+	MX6_PAD_UART5_RX_DATA__UART5_DCE_RX | MUX_PAD_CTRL(UART_PAD_CTRL),
+};
+
+#ifdef CONFIG_CONSOLE_ENABLE_GPIO
+static iomux_v3_cfg_t const ext_gpios_pads[] = {
+	MX6_PAD_GPIO1_IO04__GPIO1_IO04 | MUX_PAD_CTRL(GPI_PAD_CTRL),
+	MX6_PAD_JTAG_TDO__GPIO1_IO12 | MUX_PAD_CTRL(GPI_PAD_CTRL),
+	MX6_PAD_JTAG_TDI__GPIO1_IO13 | MUX_PAD_CTRL(GPI_PAD_CTRL),
+	MX6_PAD_JTAG_TMS__GPIO1_IO11 | MUX_PAD_CTRL(GPI_PAD_CTRL),
+	MX6_PAD_JTAG_TRST_B__GPIO1_IO15 | MUX_PAD_CTRL(GPI_PAD_CTRL),
+	MX6_PAD_JTAG_TCK__GPIO1_IO14 | MUX_PAD_CTRL(GPI_PAD_CTRL),
+};
+
+static void setup_iomux_ext_gpios(void)
+{
+	imx_iomux_v3_setup_multiple_pads(ext_gpios_pads,
+					 ARRAY_SIZE(ext_gpios_pads));
+}
+#endif /* CONFIG_CONSOLE_ENABLE_GPIO */
+
+/* micro SD */
+/*static iomux_v3_cfg_t const usdhc2_pads[] = {
+	MX6_PAD_CSI_VSYNC__USDHC2_CLK | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	MX6_PAD_CSI_HSYNC__USDHC2_CMD | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	MX6_PAD_CSI_DATA00__USDHC2_DATA0 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	MX6_PAD_CSI_DATA01__USDHC2_DATA1 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	MX6_PAD_CSI_DATA02__USDHC2_DATA2 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	MX6_PAD_CSI_DATA03__USDHC2_DATA3 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+};*/
+
+//#ifdef CONFIG_SYS_I2C_MXC
+#define PC MUX_PAD_CTRL(I2C_PAD_CTRL)
+/* I2C2 */
+/*struct i2c_pads_info i2c2_pad_info = {
+	.scl = {
+		.i2c_mode =  MX6_PAD_GPIO1_IO00__I2C2_SCL | PC,
+		.gpio_mode = MX6_PAD_GPIO1_IO00__GPIO1_IO00 | PC,
+		.gp = IMX_GPIO_NR(1, 0),
+	},
+	.sda = {
+		.i2c_mode = MX6_PAD_GPIO1_IO01__I2C2_SDA | PC,
+		.gpio_mode = MX6_PAD_GPIO1_IO01__GPIO1_IO01 | PC,
+		.gp = IMX_GPIO_NR(1, 1),
+	},
+};*/
+//#endif
+
+#ifdef CONFIG_FEC_MXC
+/*
+ * pin conflicts for fec1 and fec2, GPIO1_IO06 and GPIO1_IO07 can only
+ * be used for ENET1 or ENET2, cannot be used for both.
+ */
+static iomux_v3_cfg_t const fec1_pads[] = {
+	MX6_PAD_GPIO1_IO06__ENET1_MDIO | MUX_PAD_CTRL(MDIO_PAD_CTRL),
+	MX6_PAD_GPIO1_IO07__ENET1_MDC | MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_ENET1_TX_DATA0__ENET1_TDATA00 | MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_ENET1_TX_DATA1__ENET1_TDATA01 | MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_ENET1_TX_EN__ENET1_TX_EN | MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_ENET1_TX_CLK__ENET1_REF_CLK1 | MUX_PAD_CTRL(ENET_CLK_PAD_CTRL),
+	MX6_PAD_ENET1_RX_DATA0__ENET1_RDATA00 | MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_ENET1_RX_DATA1__ENET1_RDATA01 | MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_ENET1_RX_ER__ENET1_RX_ER | MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_ENET1_RX_EN__ENET1_RX_EN | MUX_PAD_CTRL(ENET_PAD_CTRL),
+	/*
+	 * GPIO3_IO2 is used as PHY reset in Starter Board v1 and as PHY power
+	 * enable on Starter Board v2
+	 */
+	//MX6_PAD_LCD_HSYNC__GPIO3_IO02 | MUX_PAD_CTRL(NO_PAD_CTRL),
+};
+
+static void setup_iomux_fec(void)
+{
+	imx_iomux_v3_setup_multiple_pads(fec1_pads, ARRAY_SIZE(fec1_pads));
+}
+#endif
+
+static void setup_iomux_uart(void)
+{
+	imx_iomux_v3_setup_multiple_pads(uart5_pads, ARRAY_SIZE(uart5_pads));
+}
+
+#ifdef CONFIG_FEC_MXC
+void reset_phy()
+{
+	/*
+	 * The reset line must be held low for a minimum of 100usec and cannot
+	 * be deasserted before 25ms have passed since the power supply has
+	 * reached 80% of the operating voltage. At this point of the code
+	 * we can assume the second premise is already accomplished.
+	 */
+		int phy_reset_gpio = IMX_GPIO_NR(3, 13);
+
+		/* Assert PHY reset (low) */
+		gpio_request(phy_reset_gpio, "ENET PHY Reset");
+		gpio_direction_output(phy_reset_gpio , 0);
+		udelay(100);
+		/* Deassert PHY reset (high) */
+		gpio_set_value(phy_reset_gpio, 1);
+}
+
+int board_eth_init(struct bd_info *bis)
+{
+	int ret;
+
+	setup_iomux_fec();
+
+	ret = fecmxc_initialize_multi(bis, CONFIG_FEC_ENET_DEV,
+		CONFIG_FEC_MXC_PHYADDR, IMX_FEC_BASE);
+	if (ret)
+		printf("FEC%d MXC: %s:failed\n", CONFIG_FEC_ENET_DEV, __func__);
+
+	reset_phy();
+
+	return 0;
+}
+
+static int setup_fec(int fec_id)
+{
+	struct iomuxc_gpr_base_regs *const iomuxc_gpr_regs
+		= (struct iomuxc_gpr_base_regs *) IOMUXC_GPR_BASE_ADDR;
+	int ret;
+
+	if (board_version != 1) {
+		/* Enable PHY power */
+		int phy_power_gpio = IMX_GPIO_NR(3, 2);
+
+		gpio_request(phy_power_gpio, "ENET PHY power enable");
+		gpio_direction_output(phy_power_gpio , 1);
+	}
+
+	if (0 == fec_id) {
+		if (check_module_fused(MODULE_ENET1))
+			return -1;
+
+		/* Use 50M anatop loopback REF_CLK1 for ENET1, clear gpr1[13], set gpr1[17]*/
+		clrsetbits_le32(&iomuxc_gpr_regs->gpr[1], IOMUX_GPR1_FEC1_MASK,
+				IOMUX_GPR1_FEC1_CLOCK_MUX1_SEL_MASK);
+	} else {
+		if (check_module_fused(MODULE_ENET2))
+			return -1;
+
+		/* Use 50M anatop loopback REF_CLK2 for ENET2, clear gpr1[14], set gpr1[18]*/
+		clrsetbits_le32(&iomuxc_gpr_regs->gpr[1], IOMUX_GPR1_FEC2_MASK,
+				IOMUX_GPR1_FEC2_CLOCK_MUX1_SEL_MASK);
+	}
+
+	ret = enable_fec_anatop_clock(fec_id, ENET_50MHZ);
+	if (ret)
+		return ret;
+
+	enable_enet_clk(1);
+
+	return 0;
+}
+#endif
+
+#ifdef CONFIG_USB_EHCI_MX6
+#define USB_OTHERREGS_OFFSET	0x800
+#define UCTRL_PWR_POL		(1 << 9)
+
+static iomux_v3_cfg_t const usb_otg_pads[] = {
+	MX6_PAD_GPIO1_IO00__ANATOP_OTG1_ID | MUX_PAD_CTRL(OTG_ID_PAD_CTRL),
+};
+
+/* At default the 3v3 enables the MIC2026 for VBUS power */
+static void setup_usb(void)
+{
+	imx_iomux_v3_setup_multiple_pads(usb_otg_pads,
+					 ARRAY_SIZE(usb_otg_pads));
+}
+
+int board_usb_phy_mode(int port)
+{
+	if (port == 1)
+		return USB_INIT_HOST;
+	else
+		return usb_phy_mode(port);
+}
+
+int board_ehci_hcd_init(int port)
+{
+	u32 *usbnc_usb_ctrl;
+
+	if (port > 1)
+		return -EINVAL;
+
+	usbnc_usb_ctrl = (u32 *)(USB_BASE_ADDR + USB_OTHERREGS_OFFSET +
+				 port * 4);
+
+	/* Set Power polarity */
+	setbits_le32(usbnc_usb_ctrl, UCTRL_PWR_POL);
+
+	return 0;
+}
+#endif
+
+int board_early_init_f(void)
+{
+	setup_iomux_uart();
+
+#ifdef CONFIG_CONSOLE_DISABLE
+	gd->flags |= (GD_FLG_DISABLE_CONSOLE | GD_FLG_SILENT);
+#endif
+
+	return 0;
+}
+
+#ifdef CONFIG_POWER
+int power_init_board(void)
+{
+	/* SOM power init */
+	power_init_ccimx6ul();
+
+	return 0;
+}
+#endif
+
+int board_init(void)
+{
+	/* SOM init */
+	ccimx6ul_init();
+
+	board_version = get_carrierboard_version();
+	/* Treat Starter Board version 1 (prototypes) as version 2 */
+	if (board_version == 1)
+		board_version = 2;
+	board_id = get_carrierboard_id();
+
+#ifdef CONFIG_I2C_MULTI_BUS
+	/* Setup I2C2 (Groove connector) */
+	setup_i2c(1, CONFIG_SYS_I2C_SPEED, 0x7f, &i2c2_pad_info);
+#endif
+
+#ifdef	CONFIG_FEC_MXC
+	setup_fec(CONFIG_FEC_ENET_DEV);
+#endif
+
+#ifdef CONFIG_USB_EHCI_MX6
+	setup_usb();
+#endif
+
+	return 0;
+}
+
+void platform_default_environment(void)
+{
+	char cmd[80];
+
+	som_default_environment();
+
+	/* Set $board_version variable if defined in OTP bits */
+	if (board_version > 0) {
+		sprintf(cmd, "setenv -f board_version %d", board_version);
+		run_command(cmd, 0);
+	}
+
+	/* Set $board_id variable if defined in OTP bits */
+	if (board_id > 0) {
+		sprintf(cmd, "setenv -f board_id %d", board_id);
+		run_command(cmd, 0);
+	}
+}
+
+int board_late_init(void)
+{
+#ifdef CONFIG_CONSOLE_ENABLE_GPIO
+	const char *ext_gpios[] = {
+		"GPIO1_4",	/* J8.7 */
+		"GPIO1_12",	/* J8.35 */
+		"GPIO1_13",	/* J8.12 */
+		"GPIO1_11",	/* J8.16 */
+		"GPIO1_15",	/* J8.38 */
+		"GPIO1_14",	/* J8.40 */
+	};
+	const char *ext_gpio_name = ext_gpios[CONFIG_CONSOLE_ENABLE_GPIO_NR];
+
+	setup_iomux_ext_gpios();
+
+	if (console_enable_gpio(ext_gpio_name))
+		gd->flags &= ~(GD_FLG_DISABLE_CONSOLE | GD_FLG_SILENT);
+#endif
+	/* SOM late init */
+	ccimx6ul_late_init();
+
+	/* Set default dynamic variables */
+	platform_default_environment();
+
+	set_wdog_reset((struct wdog_regs *)WDOG1_BASE_ADDR);
+
+	return 0;
+}
+
+#if defined(CONFIG_OF_BOARD_SETUP)
+/* Platform function to modify the FDT as needed */
+int ft_board_setup(void *blob, struct bd_info *bd)
+{
+	fdt_fixup_ccimx6ul(blob);
+	fdt_fixup_carrierboard(blob);
+
+	return 0;
+}
+#endif /* CONFIG_OF_BOARD_SETUP */
+
+int checkboard(void)
+{
+	print_ccimx6ul_info();
+	print_carrierboard_info();
+	printf("Boot device:  %s\n",
+	       is_boot_from_usb() ? "USB" : get_boot_device_name());
+
+	return 0;
+}
---
+++ include/configs/ccimx6ulalvic.h
@@ -0,0 +1,282 @@
+/*
+ * Copyright (C) 2016-2021 Digi International, Inc.
+ * Copyright (C) 2015 Freescale Semiconductor, Inc.
+ *
+ * Configuration settings for the Digi ConnecCore 6UL Starter board.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+#ifndef CCIMX6ULALVIC_CONFIG_H
+#define CCIMX6ULALVIC_CONFIG_H
+
+#include "ccimx6ul_common.h"
+
+#define CONFIG_BOARD_DESCRIPTION	"iHCP Manager"
+
+/* uncomment for PLUGIN mode support */
+/* #define CONFIG_USE_PLUGIN */
+
+/* uncomment for BEE support, needs to enable CONFIG_CMD_FUSE */
+/* #define CONFIG_CMD_BEE */
+
+/* NAND stuff */
+#ifdef CONFIG_NAND_MXS
+#define CONFIG_CMD_NAND_TRIMFFS
+#define CONFIG_SYS_MAX_NAND_DEVICE	1
+#define CONFIG_SYS_NAND_BASE		0x40000000
+#define CONFIG_SYS_NAND_5_ADDR_CYCLE
+#define CONFIG_SYS_NAND_ONFI_DETECTION
+#endif
+
+#define CONFIG_SYS_FSL_USDHC_NUM	1
+
+/* U-Boot Environment */
+#if defined(CONFIG_ENV_IS_IN_MMC)
+#undef CONFIG_ENV_SIZE
+#undef CONFIG_ENV_OFFSET
+#define CONFIG_ENV_SIZE			SZ_16K
+#define CONFIG_ENV_OFFSET		(8 * SZ_64K)
+#elif defined(CONFIG_ENV_IS_IN_SPI_FLASH)
+#undef CONFIG_ENV_SIZE
+#undef CONFIG_ENV_OFFSET
+#define CONFIG_ENV_SIZE			SZ_16K
+#define CONFIG_ENV_OFFSET		(768 * 1024)
+#define CONFIG_ENV_SPI_BUS		CONFIG_SF_DEFAULT_BUS
+#define CONFIG_ENV_SPI_CS		CONFIG_SF_DEFAULT_CS
+#define CONFIG_ENV_SPI_MODE		CONFIG_SF_DEFAULT_MODE
+#define CONFIG_ENV_SPI_MAX_HZ		CONFIG_SF_DEFAULT_SPEED
+#endif
+#define CONFIG_DYNAMIC_ENV_LOCATION
+#if (CONFIG_DDR_MB == 1024)
+# define CONFIG_ENV_PARTITION_SIZE	(3 * SZ_1M)
+#else
+# define CONFIG_ENV_PARTITION_SIZE	(1 * SZ_1M)
+#endif /* (CONFIG_DDR_MB == 1024) */
+/* The environment may use any good blocks within the "environment" partition */
+#define CONFIG_ENV_RANGE		CONFIG_ENV_PARTITION_SIZE
+
+/* Serial port */
+#define CONFIG_MXC_UART
+#define CONFIG_MXC_UART_BASE		UART5_BASE
+#undef CONFIG_CONS_INDEX
+#define CONFIG_CONS_INDEX		5
+#define CONSOLE_DEV			"ttymxc4"
+#define CONFIG_BAUDRATE			115200
+
+/* Ethernet */
+#define CONFIG_FEC_ENET_DEV		0
+#if (CONFIG_FEC_ENET_DEV == 0)
+#define IMX_FEC_BASE			ENET_BASE_ADDR
+#define CONFIG_FEC_MXC_PHYADDR          0x0
+#define CONFIG_ETHPRIME                 "FEC0"
+#define CONFIG_FEC_XCV_TYPE             RMII
+#elif (CONFIG_FEC_ENET_DEV == 1)
+#define IMX_FEC_BASE			ENET2_BASE_ADDR
+#define CONFIG_FEC_MXC_PHYADDR          0x1
+#define CONFIG_ETHPRIME                 "FEC1"
+#define CONFIG_FEC_XCV_TYPE             RMII
+#endif
+
+#undef CONFIG_DEFAULT_FDT_FILE
+#define CONFIG_DEFAULT_FDT_FILE		"imx6ul-" CONFIG_SYS_BOARD ".dtb"
+
+#undef CONFIG_BOOTCOMMAND
+#define CONFIG_BOOTCOMMAND \
+	"if run loadscript; then " \
+		"source ${loadaddr};" \
+	"fi;"
+
+#define CONFIG_COMMON_ENV	\
+	CONFIG_DEFAULT_NETWORK_SETTINGS \
+	CONFIG_EXTRA_NETWORK_SETTINGS \
+	ALTBOOTCMD \
+	"bootcmd_mfg=fastboot " __stringify(CONFIG_FASTBOOT_USB_DEV) "\0" \
+	"dualboot=no\0" \
+	"boot_fdt=yes\0" \
+	"bootargs_mmc_linux=setenv bootargs console=${console},${baudrate} " \
+		"${bootargs_linux} root=${mmcroot} ${mtdparts}" \
+		"${bootargs_once} ${extra_bootargs}\0" \
+	"bootargs_nfs=" \
+		"if test ${ip_dyn} = yes; then " \
+			"bootargs_ip=\"ip=dhcp\";" \
+		"else " \
+			"bootargs_ip=\"ip=\\${ipaddr}:\\${serverip}:" \
+			"\\${gatewayip}:\\${netmask}:\\${hostname}:" \
+			"eth0:off\";" \
+		"fi;\0" \
+	"bootargs_nfs_linux=run bootargs_nfs;" \
+		"setenv bootargs console=${console},${baudrate} " \
+		"${bootargs_linux} root=/dev/nfs " \
+		"${bootargs_ip} nfsroot=${serverip}:${rootpath},v3,tcp " \
+		"${mtdparts} ${bootargs_once} ${extra_bootargs}\0" \
+	"bootargs_tftp=" \
+		"if test ${ip_dyn} = yes; then " \
+			"bootargs_ip=\"ip=dhcp\";" \
+		"else " \
+			"bootargs_ip=\"ip=\\${ipaddr}:\\${serverip}:" \
+			"\\${gatewayip}:\\${netmask}:\\${hostname}:" \
+			"eth0:off\";" \
+		"fi;\0" \
+	"bootargs_tftp_linux=run bootargs_tftp;" \
+		"setenv bootargs console=${console},${baudrate} " \
+		"${bootargs_linux} root=/dev/nfs " \
+		"${bootargs_ip} nfsroot=${serverip}:${rootpath},v3,tcp " \
+		"${mtdparts} ${bootargs_once} ${extra_bootargs}\0" \
+	"console=" CONSOLE_DEV "\0" \
+	"dboot_kernel_var=zimage\0" \
+	"fdt_addr=0x83000000\0" \
+	"fdt_file=" CONFIG_DEFAULT_FDT_FILE "\0" \
+	"fdt_high=0xffffffff\0"	  \
+	"initrd_addr=0x83800000\0" \
+	"initrd_file=uramdisk.img\0" \
+	"initrd_high=0xffffffff\0" \
+	"update_addr=" __stringify(CONFIG_DIGI_UPDATE_ADDR) "\0" \
+	"mmcroot=" CONFIG_MMCROOT " rootwait rw\0" \
+	"recovery_file=recovery.img\0" \
+	"script=boot.scr\0" \
+	"uboot_file=u-boot.imx\0" \
+	"zimage=zImage-" CONFIG_SYS_BOARD ".bin\0"
+
+#if defined(CONFIG_NAND_BOOT)
+
+#define ROOTARGS_SINGLEMTDSYSTEM_UBIFS \
+	"ubi.mtd=" SYSTEM_PARTITION " " \
+	"root=ubi0:${rootfsvol} " \
+	"rootfstype=ubifs rw"
+#define ROOTARGS_MULTIMTDSYSTEM_UBIFS \
+	"ubi.mtd=${mtdbootpart} " \
+	"ubi.mtd=${mtdrootfspart} " \
+	"root=ubi1:${rootfsvol} " \
+	"rootfstype=ubifs rw"
+#define ROOTARGS_SINGLEMTDSYSTEM_SQUASHFS \
+	"ubi.mtd=" SYSTEM_PARTITION " " \
+	"ubi.block=0,2 root=/dev/ubiblock0_2 " \
+	"rootfstype=squashfs ro"
+#define ROOTARGS_MULTIMTDSYSTEM_SQUASHFS \
+	"ubi.mtd=${mtdbootpart} " \
+	"ubi.mtd=${mtdrootfspart} " \
+	"ubi.block=1,0 root=/dev/ubiblock1_0 " \
+	"rootfstype=squashfs ro"
+
+#define MTDPART_ENV_SETTINGS \
+	"mtdbootpart=" LINUX_PARTITION "\0" \
+	"mtdrootfspart=" ROOTFS_PARTITION "\0" \
+	"singlemtdsys=no\0" \
+	"rootfsvol=" ROOTFS_PARTITION "\0" \
+	"bootargs_nand_linux=" \
+		"if test \"${singlemtdsys}\" = yes; then " \
+			"if test \"${rootfstype}\" = squashfs; then " \
+				"setenv rootargs " ROOTARGS_SINGLEMTDSYSTEM_SQUASHFS ";" \
+			"else " \
+				"setenv rootargs " ROOTARGS_SINGLEMTDSYSTEM_UBIFS ";" \
+			"fi;" \
+		"else " \
+			"if test \"${rootfstype}\" = squashfs; then " \
+				"setenv rootargs " ROOTARGS_MULTIMTDSYSTEM_SQUASHFS ";" \
+			"else " \
+				"setenv rootargs " ROOTARGS_MULTIMTDSYSTEM_UBIFS ";" \
+			"fi;" \
+		"fi;" \
+		"setenv bootargs console=${console},${baudrate} " \
+			"${bootargs_linux} ${mtdparts} " \
+			"${rootargs} " \
+			"${bootargs_once} ${extra_bootargs};\0" \
+	"loadscript=" \
+		"if test \"${dualboot}\" = yes; then " \
+			"if test -z \"${active_system}\"; then " \
+				"setenv active_system " LINUX_A_PARTITION ";" \
+			"fi;" \
+			"setenv mtdbootpart ${active_system};" \
+		"else " \
+			"if test -z \"${mtdbootpart}\" || " \
+			"   test \"${mtdbootpart}\" = " LINUX_A_PARTITION " || " \
+			"   test \"${mtdbootpart}\" = " LINUX_B_PARTITION "; then " \
+				"setenv mtdbootpart " LINUX_PARTITION ";" \
+			"fi;" \
+		"fi;" \
+		"if test \"${singlemtdsys}\" = yes; then " \
+			"if ubi part " SYSTEM_PARTITION "; then " \
+				"if ubifsmount ubi0:${mtdbootpart}; then " \
+					"ubifsload ${loadaddr} ${script};" \
+				"fi;" \
+			"fi;" \
+		"else " \
+			"if ubi part ${mtdbootpart}; then " \
+				"if ubifsmount ubi0:${mtdbootpart}; then " \
+					"ubifsload ${loadaddr} ${script};" \
+				"fi;" \
+			"fi;" \
+		"fi;\0" \
+	"recoverycmd=" \
+		"setenv mtdbootpart " RECOVERY_PARTITION ";" \
+		"boot\0"
+#define DUALBOOT_ENV_SETTINGS \
+	"linux_a=" LINUX_A_PARTITION "\0" \
+	"linux_b=" LINUX_B_PARTITION "\0" \
+	"rootfsvol_a=" ROOTFS_A_PARTITION "\0" \
+	"rootfsvol_b=" ROOTFS_B_PARTITION "\0" \
+	"active_system=" LINUX_A_PARTITION "\0"
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	CONFIG_COMMON_ENV \
+	CONFIG_ENV_MTD_SETTINGS \
+	DUALBOOT_ENV_SETTINGS \
+	MTDPART_ENV_SETTINGS \
+	"bootargs_linux=\0" \
+	"install_linux_fw_sd=if load mmc 0 ${loadaddr} install_linux_fw_sd.scr;then " \
+			"source ${loadaddr};" \
+		"fi;\0" \
+	"install_linux_fw_usb=usb start;" \
+		"if load usb 0 ${loadaddr} install_linux_fw_usb.scr;then " \
+			"source ${loadaddr};" \
+		"fi;\0" \
+	"linux_file=core-image-base-" CONFIG_SYS_BOARD ".boot.ubifs\0" \
+	"rootfs_file=core-image-base-" CONFIG_SYS_BOARD ".ubifs\0" \
+	""	/* end line */
+#else
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	CONFIG_COMMON_ENV \
+	"bootcmd_mfg=fastboot " __stringify(CONFIG_FASTBOOT_USB_DEV) "\0" \
+	"loadscript=load mmc ${mmcbootdev}:${mmcpart} ${loadaddr} ${script}\0" \
+	"mmcdev="__stringify(CONFIG_SYS_MMC_ENV_DEV)"\0" \
+	"mmcpart=1\0" \
+	"mmcautodetect=yes\0" \
+	"mmcargs=setenv bootargs console=${console},${baudrate} " \
+		"${mtdparts} " \
+		"root=${mmcroot}\0" \
+	""	/* end line */
+#endif
+
+#define CONFIG_MMCROOT			"/dev/mmcblk1p2"  /* USDHC2 */
+
+/* Carrier board version and ID commands */
+#define CONFIG_CMD_BOARD_VERSION
+#define CONFIG_CMD_BOARD_ID
+
+/* Carrier board version in OTP bits */
+#define CONFIG_HAS_CARRIERBOARD_VERSION
+#ifdef CONFIG_HAS_CARRIERBOARD_VERSION
+/* The carrier board version is stored in Bank 4 Word 6 (GP1)
+ * in bits 3..0 */
+#define CONFIG_CARRIERBOARD_VERSION_ON_OTP
+#define CONFIG_CARRIERBOARD_VERSION_BANK	4
+#define CONFIG_CARRIERBOARD_VERSION_WORD	6
+#define CONFIG_CARRIERBOARD_VERSION_MASK	0xf
+#define CONFIG_CARRIERBOARD_VERSION_OFFSET	0
+#endif /* CONFIG_HAS_CARRIERBOARD_VERSION */
+
+/* Carrier board ID in OTP bits */
+#define CONFIG_HAS_CARRIERBOARD_ID
+#ifdef CONFIG_HAS_CARRIERBOARD_ID
+/* The carrier board ID is stored in Bank 4 Word 6 (GP1)
+ * in bits 11..4 */
+#define CONFIG_CARRIERBOARD_ID_ON_OTP
+#define CONFIG_CARRIERBOARD_ID_BANK	4
+#define CONFIG_CARRIERBOARD_ID_WORD	6
+#define CONFIG_CARRIERBOARD_ID_MASK	0xff
+#define CONFIG_CARRIERBOARD_ID_OFFSET	4
+#endif /* CONFIG_HAS_CARRIERBOARD_ID */
+
+/* UBI and UBIFS support */
+
+#endif /* CCIMX6ULALVIC_CONFIG_H */
