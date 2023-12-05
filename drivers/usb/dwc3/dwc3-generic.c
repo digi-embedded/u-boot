digi-embedded/u-boot

commit 868d58f69c7ceda5c886293cf15f4e39493517a2

usb: dwc3: Fix non-usb3 configurations
Missing nodes may also be signaled via -ENODATA. We need to check for
that to prevent failing in non-usb3 setups.

Furthermore, dev.phy must be NULL'ed in case usb3-phy was not found.

Fixes: 142d50fbce7c ("usb: dwc3: Add support for usb3-phy PHY configuration")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

drivers/usb/dwc3/dwc3-generic.c
@@ -468,9 +468,11 @@ static int dwc3_glue_probe(struct udevice *dev)
 		ret = generic_phy_init(&phy);
 		if (ret)
 			return ret;
-	} else if (ret != -ENOENT) {
+	} else if (ret != -ENOENT && ret != -ENODATA) {
 		debug("could not get phy (err %d)\n", ret);
 		return ret;
+	} else {
+		phy.dev = NULL;
 	}
 
 	glue->regs = dev_read_addr(dev);

commit 58aea54f98d487eb2c9c1f9caf6f8943efb3380c

usb: dwc3: Fix for usb3 configuration with no usb3-phy
Missing property may also be signaled via -EINVAL
Fixes: 868d58f69c7c ("usb: dwc3: Fix non-usb3 configurations")
Fixes: 142d50fbce7c ("usb: dwc3: Add support for usb3-phy PHY configuration")

Signed-off-by: Pankaj Dev <pankaj.dev@st.com>
Change-Id: I4db39d0d853d6bfcf3bae9b0bb4b7ed181515ca4
Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/u-boot/+/303905
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
Reviewed-by: Fabrice GASNIER <fabrice.gasnier@foss.st.com>
Reviewed-by: Patrice CHOTARD <patrice.chotard@foss.st.com>
Domain-Review: Fabrice GASNIER <fabrice.gasnier@foss.st.com>

drivers/usb/dwc3/dwc3-generic.c
@@ -567,7 +567,7 @@ static int dwc3_glue_probe(struct udevice *dev)
 		ret = generic_phy_init(&phy);
 		if (ret)
 			return ret;
-	} else if (ret != -ENOENT && ret != -ENODATA) {
+	} else if (ret != -ENOENT && ret != -ENODATA && ret != -EINVAL) {
 		debug("could not get phy (err %d)\n", ret);
 		return ret;
 	} else {