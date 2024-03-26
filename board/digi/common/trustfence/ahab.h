8660c416f7af89b58e30426735bee5e0ed55ef42
Javier Viguera <javier.viguera@digi.com>
2024-03-26 17:20:14 +0100

    ccimx8x: move trustfence related functions out of the platform code
    This commit does not make any functional or even code changes. It only
    moves the get_dek_blob_offset, get_dek_blob_size, and get_dek_blob
    functions to a different file.
    
    AHAB-based ccimx93 platform will use this code in the following commits.
    
    https://onedigi.atlassian.net/browse/DEL-8945
    
    Signed-off-by: Javier Viguera <javier.viguera@digi.com>

---
+++ board/digi/common/trustfence/ahab.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2024 Digi International Inc
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ */
+
+int get_dek_blob_offset(char *address, u32 *offset);
+int get_dek_blob_size(char *address, u32 *size);
+int get_dek_blob(char *output, u32 *size);
