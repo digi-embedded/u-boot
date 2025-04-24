6ba3d02c9188b80f5bb8cd79e85e6746cb3c51e2
Gabriel Valcazar <gabriel.valcazar@digi.com>
2025-04-24 13:13:38 +0200

    Revert "trustfence: env: remove redundant sec initialization"
    On the ccimx6ul, there's a corner case that prevents the system from booting.
    The conditions are:
    
        * Secure boot is enabled and the bootscript is signed
        * Environment encryption is enabled
        * Dualboot is enabled
        * The bootscript's dualboot logic triggers an "env save" to sanitize boot
          parameters
    
    When this happens, the CAAM is used to encrypt the environment shortly after
    it's used to authenticate the bootscript, which triggers a blob encapsulation
    error. The CAAM clk and the sec subsystem have to be initialized between these
    two operations (authentication and encryption) in order to avoid the error.
    
    This reverts commit 833a7732fa3c8c3eef4518795445ce9e5adc0b0e.
    
    https://onedigi.atlassian.net/browse/DEL-9591
    
    Signed-off-by: Gabriel Valcazar <gabriel.valcazar@digi.com>

--- board/digi/common/trustfence/env.c
+++ board/digi/common/trustfence/env.c
@@ -14,6 +14,11 @@
 
 #ifdef CONFIG_CAAM_ENV_ENCRYPT
 #include <fsl_sec.h>
+#if defined(CONFIG_ARCH_MX6) || defined(CONFIG_ARCH_MX7) || \
+	defined(CONFIG_ARCH_MX7ULP) || defined(CONFIG_ARCH_IMX8M)
+#include <asm/arch/clock.h>
+#include <asm/arch/imx-regs.h>
+#endif
 #endif
 
 #ifdef CONFIG_OPTEE_ENV_ENCRYPT
@@ -89,6 +94,14 @@ int env_crypt(env_t * env, const int enc)
 		goto freesrc;
 	}
 	memcpy(src_ptr, data, ENV_SIZE);
+
+#if defined(CONFIG_ARCH_MX6) || defined(CONFIG_ARCH_MX7) || \
+	defined(CONFIG_ARCH_MX7ULP) || defined(CONFIG_ARCH_IMX8M)
+	hab_caam_clock_enable(1);
+	u32 out_jr_size = sec_in32(CFG_SYS_FSL_JR0_ADDR + FSL_CAAM_ORSR_JRa_OFFSET);
+	if (out_jr_size != FSL_CAAM_MAX_JR_SIZE)
+		sec_init();
+#endif
 
 	if (enc) {
 		printf("Encrypting... ");
