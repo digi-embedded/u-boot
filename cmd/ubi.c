57e67fc9324fd4dba1717365120acd60ee52f438
Francisco Gil <francisco.gilmartinez@digi.com>
2025-05-09 14:09:11 +0200

    cmd: ubi: reduce verbosity on partition selection
    Reference: af298f3dcdae31b49791ff7aa1a6d75a27e57f3a
    
    Signed-off-by: Francisco Gil <francisco.gilmartinez@digi.com>

--- cmd/ubi.c
+++ cmd/ubi.c
@@ -824,7 +824,6 @@ int ubi_part(char *part_name, const char *vid_header_offset)
 	int err = 0;
 
 	if (ubi && ubi->mtd && !strcmp(ubi->mtd->name, part_name)) {
-		printf("UBI partition '%s' already selected\n", part_name);
 		return 0;
 	}
 
