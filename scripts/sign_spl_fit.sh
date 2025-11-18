3262ef5613c4b5ea4c25f093435ec469341281c4
Gabriel Valcazar <gabriel.valcazar@digi.com>
2025-11-18 11:35:19 +0100

    scripts: sign_spl_fit.sh: use mac_dump tool to obtain nonce/MAC parameters
    We were using an ad-hoc method with a hardcoded nonce/MAC length, but NXP
    provides a tool for this since CST v3.4.1, so use it instead.
    
    CSF size hasn't been used in nonce/MAC offset calculations for some time, so
    there's no need to calculate it or use it anymore.
    
    https://onedigi.atlassian.net/browse/DEL-9334
    
    Signed-off-by: Gabriel Valcazar <gabriel.valcazar@digi.com>

--- scripts/sign_spl_fit.sh
+++ scripts/sign_spl_fit.sh
@@ -368,25 +368,21 @@ else
 	fi
 
 	# Create final CSF for SPL
-	hab_tag_mac="AC00244"
-	nonce_mac_size="36"
-	csf_size="$(stat -L -c %s csf_spl_enc.bin)"
-	nonce_offset="$(hexdump -ve '1/1 "%.2X"' csf_spl_enc.bin | grep -ob -e "${hab_tag_mac}" | awk -F: '{gsub(/ /, ""); print int($1/2)}')"
-	echo "SPL ENC csf_size: ${csf_size} / nonce_offset: ${nonce_offset}"
+	nonce_offset="$(mac_dump csf_spl_enc.bin | awk '/offset:/ { printf $3 }' | xargs printf "%d")"
+	nonce_mac_size="$(mac_dump csf_spl_enc.bin | awk '/length:/ { printf $3 }' | xargs printf "%d")"
+	echo "SPL ENC nonce_offset: ${nonce_offset}"
 	dd if=csf_spl_enc.bin of=noncemac.bin bs=1 skip=${nonce_offset} count="${nonce_mac_size}" conv=notrunc
-	csf_size="$(stat -L -c %s csf_spl_sign_enc.bin)"
-	nonce_offset="$(hexdump -ve '1/1 "%.2X"' csf_spl_sign_enc.bin | grep -ob -e "${hab_tag_mac}" | awk -F: '{gsub(/ /, ""); print int($1/2)}')"
-	echo "SPL SIGN ENC csf_size: ${csf_size} / nonce_offset: ${nonce_offset}"
+	nonce_offset="$(mac_dump csf_spl_sign_enc.bin | awk '/offset:/ { printf $3 }' | xargs printf "%d")"
+	echo "SPL SIGN ENC nonce_offset: ${nonce_offset}"
 	dd if=noncemac.bin of=csf_spl_sign_enc.bin bs=1 seek=${nonce_offset} count="${nonce_mac_size}" conv=notrunc
 
 	# Create final CSF for FIT
-	csf_size="$(stat -L -c %s csf_fit_enc.bin)"
-	nonce_offset="$(hexdump -ve '1/1 "%.2X"' csf_fit_enc.bin | grep -ob -e "${hab_tag_mac}" | awk -F: '{gsub(/ /, ""); print int($1/2)}')"
-	echo "FIT ENC csf_size: ${csf_size} / nonce_offset: ${nonce_offset}"
+	nonce_offset="$(mac_dump csf_fit_enc.bin | awk '/offset:/ { printf $3 }' | xargs printf "%d")"
+	nonce_mac_size="$(mac_dump csf_fit_enc.bin | awk '/length:/ { printf $3 }' | xargs printf "%d")"
+	echo "FIT ENC nonce_offset: ${nonce_offset}"
 	dd if=csf_fit_enc.bin of=noncemac.bin bs=1 skip=${nonce_offset} count="${nonce_mac_size}" conv=notrunc
-	csf_size="$(stat -L -c %s csf_fit_sign_enc.bin)"
-	nonce_offset="$(hexdump -ve '1/1 "%.2X"' csf_fit_sign_enc.bin | grep -ob -e "${hab_tag_mac}" | awk -F: '{gsub(/ /, ""); print int($1/2)}')"
-	echo "FIT SIGN ENC csf_size: ${csf_size} / nonce_offset: ${nonce_offset}"
+	nonce_offset="$(mac_dump csf_fit_sign_enc.bin | awk '/offset:/ { printf $3 }' | xargs printf "%d")"
+	echo "FIT SIGN ENC nonce_offset: ${nonce_offset}"
 	dd if=noncemac.bin of=csf_fit_sign_enc.bin bs=1 seek=${nonce_offset} count="${nonce_mac_size}" conv=notrunc
 
 	cp flash-spl-fit-enc.bin "${TARGET}"
