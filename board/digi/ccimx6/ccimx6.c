b2cdd133042cc6380a52f9f923a4061a1b886c8f
Gonzalo Ruiz <Gonzalo.Ruiz@digi.com>
2026-03-02 10:41:31 +0100

    hwid: drop 'sense' and 'override' fuse operations
    Now that we have the ENV HWID to override the FUSE HWID, reading and
    storing the HWID into fuse shadow registers is redundant. Drop the
    specific functions for that.
    
    Repurpose the 'read' function to sense the the fuse registers.
    
    Now the only operations that will remain for FUSE HWID are:
    - read: read the fuse registers
    - prog: program the fuse registers
    - lock: lock the fuse registers
    
    https://onedigi.atlassian.net/browse/DEL-9123
    
    Signed-off-by: Gonzalo Ruiz <Gonzalo.Ruiz@digi.com>

--- board/digi/ccimx6/ccimx6.c
+++ board/digi/ccimx6/ccimx6.c
@@ -1872,7 +1872,7 @@ void som_default_environment(void)
 void board_update_hwid(bool is_fuse)
 {
 	/* Update HWID-related variables in environment */
-	int ret = is_fuse ? board_sense_hwid(&my_hwid) : board_read_hwid(&my_hwid);
+	int ret = is_fuse ? board_read_hwid(&my_hwid) : hwid_env_read(&my_hwid);
 
 	if (ret)
 		printf("Cannot read HWID\n");
--- board/digi/ccimx9/ccimx9.c
+++ board/digi/ccimx9/ccimx9.c
@@ -263,7 +263,7 @@ void board_update_hwid(bool is_fuse)
 {
 	/* Update HWID-related variables in MCA and environment */
 	int ret =
-	    is_fuse ? board_sense_hwid(&my_hwid) : board_read_hwid(&my_hwid);
+	    is_fuse ? board_read_hwid(&my_hwid) : hwid_env_read(&my_hwid);
 
 	if (ret)
 		printf("Cannot read HWID\n");
