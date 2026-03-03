4202e265b782c0ad45d84726cb5db223006cd85f
Gonzalo Ruiz <Gonzalo.Ruiz@digi.com>
2026-03-03 15:25:45 +0100

    env: add platform hook for actions on environment load
    Add a hook to let platform-specific code do actions immediately
    after successfully loading the environment.
    
    https://onedigi.atlassian.net/browse/DEL-9123
    
    Signed-off-by: Gonzalo Ruiz <Gonzalo.Ruiz@digi.com>

--- env/env.c
+++ env/env.c
@@ -163,6 +163,11 @@ static struct env_driver *env_driver_lookup(enum env_operation op, int prio)
 	return drv;
 }
 
+__weak void platform_loaded_environment(void)
+{
+	return;
+}
+
 int env_load(void)
 {
 	struct env_driver *drv;
@@ -193,6 +198,9 @@ int env_load(void)
 		if (!ret) {
 			printf("OK\n");
 			gd->env_load_prio = prio;
+
+			/* Platform-specific actions on successfully loaded environment */
+			platform_loaded_environment();
 
 			return 0;
 		} else if (ret == -ENOMSG) {
