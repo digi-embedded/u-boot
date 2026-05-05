digi-embedded/u-boot

commit 7f298aada4b7fc09c803a11e76c16370d43f5f51

env: add environment encryption/decryption for the CCMP2
This commit adds enviroment encryption/decryption support for
the CCMP2 platform. And gereralize the build configuration.

Signed-off-by: Mike Engel <Mike.Engel@digi.com>

https://onedigi.atlassian.net/browse/DEL-10029
(cherry picked from commit 7b4c7f33ab44b2923a8be371b831f6b85899cf16)

board/digi/common/env.c
@@ -15,7 +15,7 @@
 #include <fsl_caam.h>
 #include <asm/mach-imx/hab.h>
 #include "../board/digi/common/trustfence.h"
-#elif CONFIG_ENV_AES_CCMP1
+#elif CONFIG_OPTEE_ENV_ENCRYPT
 #if !defined(CONFIG_AES_KEY_LENGTH)
 #define CONFIG_AES_KEY_LENGTH ""
 #endif
@@ -64,18 +64,22 @@ err:
 	free(buffer);
 	return ret;
 }
-#elif CONFIG_ENV_AES_CCMP1
+#elif CONFIG_OPTEE_ENV_ENCRYPT
 int env_aes_cbc_crypt(env_t *env, const int enc)
 {
 	unsigned char *data = env->data;
 	int ret = 0;
+#if defined(CONFIG_CCMP1)
 	/* get NAND geometrics */
 	struct mtd_info *mtd = get_nand_dev_by_index(0);
-
+	size_t size = mtd->erasesize
+#else
+	size_t size = CONFIG_ENV_SIZE;
+#endif
 	if (enc) {
-		crypt_cipher_data(TA_AES_MODE_ENCODE, data, mtd->erasesize);
+		crypt_cipher_data(TA_AES_MODE_ENCODE, data, size);
 	} else {
-		crypt_cipher_data(TA_AES_MODE_DECODE, data, mtd->erasesize);
+		crypt_cipher_data(TA_AES_MODE_DECODE, data, size);
 	}
 
 	return ret;

Kconfig
@@ -79,23 +79,6 @@ config DEPRECATED
 	  code that relies on deprecated features that will be removed and
 	  the conversion deadline has passed.
 
-config ENV_AES_CAAM_KEY
-	bool "Encrypt U-Boot environment with CAAM internal key"
-	depends on HAS_CAAM
-	help
-	  Use the internal CAAM secure key for environment encryption which
-	  uses AES-256-CCM.
-
-	  This setting is only supported in closed devices (those which can
-	  only boot signed U-Boot images). It has no effect on open devices.
-
-config ENV_AES_CCMP1
-	bool "Encrypt U-Boot environment for CCMP1 platform"
-	depends on CCMP1
-	help
-	  Use the STM32MP cryp AES support for environment encryption which
-	  can use AES-128, AES-192 and AES-256.
-
 config LOCALVERSION
 	string "Local version - append to U-Boot release"
 	help

board/digi/ccmp1/ccmp1.c
@@ -121,10 +121,11 @@ void fdt_fixup_ccmp1(void *fdt)
 	fdt_fixup_uboot_info(fdt);
 
 	/* Add DT entry to detect environment encryption in Linux */
-#ifdef CONFIG_ENV_AES_CCMP1
-	do_fixup_by_path(fdt, "/", "digi,uboot-env,encrypted", NULL, 0, 1);
-	do_fixup_by_path(fdt, "/", "digi,uboot-env,encrypted-optee", NULL, 0, 1);
-#endif
+	if (IS_ENABLED(CONFIG_ENV_ENCRYPT))
+		do_fixup_by_path(fdt, "/", "digi,uboot-env,encrypted", NULL, 0, 1);
+	if (IS_ENABLED(CONFIG_OPTEE_ENV_ENCRYPT))
+		do_fixup_by_path(fdt, "/", "digi,uboot-env,encrypted-optee", NULL, 0, 1);
+
 	fdt_fixup_memory_node_ccmp1(fdt);
 }
 

board/digi/ccmp13-dvk/Makefile
@@ -16,7 +16,7 @@ endif
 
 obj-y += ../ccmp1/ccmp1.o
 
-ifdef CONFIG_ENV_AES_CCMP1
+ifdef CONFIG_OPTEE_ENV_ENCRYPT
 obj-y += ../ccmp1/ta_ccmp1.o
 endif
 

board/digi/ccmp15-dvk/Makefile
@@ -17,7 +17,7 @@ endif
 obj-y += ../ccmp1/ccmp1.o
 obj-$(CONFIG_CUSTOM_HUK) += ../ccmp1/ccmp15_huk.o
 
-ifdef CONFIG_ENV_AES_CCMP1
+ifdef CONFIG_OPTEE_ENV_ENCRYPT
 obj-y += ../ccmp1/ta_ccmp1.o
 endif
 

board/digi/ccmp2/ccmp2.c
@@ -110,9 +110,10 @@ void fdt_fixup_ccmp2(void *fdt)
 	fdt_fixup_uboot_info(fdt);
 
 	/* Add DT entry to detect environment encryption in Linux */
-#ifdef CONFIG_ENV_AES_CCMP1
-	do_fixup_by_path(fdt, "/", "digi,uboot-env,encrypted", NULL, 0, 1);
-#endif
+	if (IS_ENABLED(CONFIG_ENV_ENCRYPT))
+		do_fixup_by_path(fdt, "/", "digi,uboot-env,encrypted", NULL, 0, 1);
+	if (IS_ENABLED(CONFIG_OPTEE_ENV_ENCRYPT))
+		do_fixup_by_path(fdt, "/", "digi,uboot-env,encrypted-optee", NULL, 0, 1);
 }
 
 bool board_has_emmc(void)

board/digi/ccmp25-dvk/Makefile
@@ -20,3 +20,7 @@ ifeq ($(CONFIG_ARCH_STM32MP),y)
 obj-$(CONFIG_SET_DFU_ALT_INFO) += ../../st/common/stm32mp_dfu.o
 obj-$(CONFIG_$(SPL_)DFU_VIRT) += ../../st/common/stm32mp_dfu_virt.o
 endif
+
+ifdef CONFIG_OPTEE_ENV_ENCRYPT
+obj-y += ../ccmp1/ta_ccmp1.o
+endif

board/digi/common/Kconfig
@@ -207,3 +207,31 @@ config CMD_TRUSTFENCE_STM
 	default y if CCMP1
 	default y if CCMP2
 endif
+
+config ENV_ENCRYPT
+	bool "Encrypt u-boot environment using the hardware unique key (HUK)"
+	select MD5
+	help
+	  Implementation uses CAAM or Optee depending on the platform.
+
+	  This setting only has an effect on closed devices (those that can
+	  only boot signed U-Boot images). It has no effect on open devices.
+
+config ENV_AES_CAAM_KEY
+	bool "Encrypt U-Boot environment with CAAM internal key"
+	depends on HAS_CAAM
+	depends on ENV_ENCRYPT
+	help
+	  Use the internal CAAM secure key for environment encryption which
+	  uses AES-256-CCM.
+
+	  This setting is only supported in closed devices (those which can
+	  only boot signed U-Boot images). It has no effect on open devices.
+
+config OPTEE_ENV_ENCRYPT
+	bool
+	depends on ENV_ENCRYPT
+	depends on CCMP1 || CCMP2
+	default ENV_ENCRYPT
+	help
+	  Encrypt U-Boot environment using Optee

commit 381869b8f35179922585377518edd4b052911506

env: fix typo in env_aes_cbc_crypt()
Commit 7f298aada4b7fc09c803a11e76c16370d43f5f51 ("env: add environment
encryption/decryption for the CCMP2") introduced a typo affecting CCMP1
platforms.

Signed-off-by: Arturo Buzarra <arturo.buzarra@digi.com>

board/digi/common/env.c
@@ -72,7 +72,7 @@ int env_aes_cbc_crypt(env_t *env, const int enc)
 #if defined(CONFIG_CCMP1)
 	/* get NAND geometrics */
 	struct mtd_info *mtd = get_nand_dev_by_index(0);
-	size_t size = mtd->erasesize
+	size_t size = mtd->erasesize;
 #else
 	size_t size = CONFIG_ENV_SIZE;
 #endif