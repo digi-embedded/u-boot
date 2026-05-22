2e0b3822387da862e33abaf7cc757a0ea5b1ef1e
Gonzalo Ruiz <Gonzalo.Ruiz@digi.com>
2026-05-22 17:38:55 +0200

    ccimx95: disable Type-C controller
    Disable Type-C controller support on the CC95 platform, configuring the
    port to operate as high-speed by default.
    
    This allows fastboot operation on carrier board designs that do not have
    a Type-C controller regardless of the position the USB cable is
    connected.
    
    https://onedigi.atlassian.net/browse/DEL-10114
    
    Signed-off-by: Gonzalo Ruiz <Gonzalo.Ruiz@digi.com>

--- board/digi/ccimx95-dvk/ccimx95-dvk.c
+++ board/digi/ccimx95-dvk/ccimx95-dvk.c
@@ -141,11 +141,7 @@ static void setup_typec(void)
 #define PHY_CTRL6_ALT_CLK_SEL		BIT(0)
 
 static struct dwc3_device dwc3_device_data = {
-#ifdef CONFIG_SPL_BUILD
 	.maximum_speed = USB_SPEED_HIGH,
-#else
-	.maximum_speed = USB_SPEED_SUPER,
-#endif
 	.base = USB1_BASE_ADDR,
 	.dr_mode = USB_DR_MODE_PERIPHERAL,
 	.index = 0,
